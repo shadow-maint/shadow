b1a0769d3d6afcfd19688e42f2adf8e5f988332d
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2008-03-08 22:52:44 +0000

    * lib/commonio.c (commonio_remove): Fail when the name to be   removed is used by different entries (like commonio_update does). * NEWS: This fix the behavior of groupdel when the system is not   configured to support split group but different group entries   have the name of the group to be deleted.

--- lib/commonio.c
+++ lib/commonio.c
@@ -886,6 +886,10 @@ int commonio_remove (struct commonio_db *db, const char *name)
 		errno = ENOENT;
 		return 0;
 	}
+	if (next_entry_by_name (db, p->next, name)) {
+		fprintf (stderr, _("Multiple entries named '%s' in %s. Please fix this with pwck or grpck.\n"), name, db->filename);
+		return 0;
+	}
 
 	commonio_del_entry (db, p);
 
