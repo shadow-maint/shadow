ae75a8c0c12069d0f9212b04e0f268f8a5d0ec69
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2008-05-25 20:37:51 +0000

    Allow usage of booleans in the source.

--- lib/defines.h
+++ lib/defines.h
@@ -321,4 +321,20 @@ extern char *strerror ();
 # define unused
 #endif
 
+#if HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# if ! HAVE__BOOL
+#  ifdef __cplusplus
+typedef bool _Bool;
+#  else
+typedef unsigned char _Bool;
+#  endif
+# endif
+# define bool _Bool
+# define false 0
+# define true 1
+# define __bool_true_false_are_defined 1
+#endif
+
 #endif				/* _DEFINES_H_ */
