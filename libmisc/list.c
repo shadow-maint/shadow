d2c11f8beefb8539f150c99c6a03e86715361af2
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2008-08-31 17:30:12 +0000

    * libmisc/list.c: Remove historical comment.

--- libmisc/list.c
+++ libmisc/list.c
@@ -29,9 +29,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* Removed duplicated code from gpmain.c, useradd.c, userdel.c and
-   usermod.c.  --marekm */
-
 #include <config.h>
 
 #ident "$Id$"
