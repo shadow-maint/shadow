6b14a0d08cfb7772b9de95ee949dfd82022915f7
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2010-03-18 19:23:00 +0000

    * lib/tcbfuncs.h: Re-indent. 	* lib/tcbfuncs.h: Explicitly indicate functiontakinga void 	parameter.

--- lib/tcbfuncs.h
+++ lib/tcbfuncs.h
@@ -8,12 +8,12 @@ typedef enum {
 	SHADOWTCB_SUCCESS = 1
 } shadowtcb_status;
 
-extern shadowtcb_status shadowtcb_drop_priv();
-extern shadowtcb_status shadowtcb_gain_priv();
-extern shadowtcb_status shadowtcb_set_user(const char *name);
-extern shadowtcb_status shadowtcb_remove(const char *name);
-extern shadowtcb_status shadowtcb_move(/*@null@*/const char *user_newname,
-                                       uid_t user_newid);
-extern shadowtcb_status shadowtcb_create(const char *name, uid_t uid);
+extern shadowtcb_status shadowtcb_drop_priv (void);
+extern shadowtcb_status shadowtcb_gain_priv (void);
+extern shadowtcb_status shadowtcb_set_user (const char *name);
+extern shadowtcb_status shadowtcb_remove (const char *name);
+extern shadowtcb_status shadowtcb_move (/*@null@*/const char *user_newname,
+                                        uid_t user_newid);
+extern shadowtcb_status shadowtcb_create (const char *name, uid_t uid);
 
 #endif
