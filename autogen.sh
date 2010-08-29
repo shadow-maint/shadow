91f972bb59d27772268b3a2415e8dce8dca7664d
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2010-08-29 16:42:25 +0000

    * autogen.sh: Expand autoreconf to avoid running autopoint.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
-autoreconf -v -f --install || exit 1
+
+# FIXME: Find a way to run autoreconf again.
+# autopoint create mess in man/po
+#autoreconf -v -f --install || exit 1
+
+#autoreconf executes the following
+#autopoint --force
+aclocal --force 
+libtoolize --copy --force
+/usr/bin/autoconf --force
+/usr/bin/autoheader --force
+automake --add-missing --copy --force-missing
+
 ./configure \
 	CFLAGS="-O2 -Wall" \
 	--enable-man \
