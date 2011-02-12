0398323a60b58a493c6a2093e98a6005894f1c51
nekral-guest <nekral-guest@5a98b0ae-9ef6-0310-add3-de5d479b70d7>
2011-02-12 14:57:37 +0000

    * autogen.sh: Restore original autoreconf (see 2010-08-29's change 	for autogen.sh) 	* configure.in, man/po/Makefile.in.in, man/po/Makefile.in: 	Makefile.in.in replaced by Makefile.in to avoid config.status 	issues when there are multiple po directories. 	* man/po/LINGUAS: Adapted to above change. 	* man/po/POTFILES.in, man/po/XMLFILES: POTFILES.in replaced by 	XMLFILES. 	* man/Makefile.am: generate_mans.deps shall be included by make, 	not automake.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,6 @@
 #! /bin/sh
 
-# FIXME: Find a way to run autoreconf again.
-# autopoint create mess in man/po
-#autoreconf -v -f --install || exit 1
-
-#autoreconf executes the following
-#autopoint --force
-aclocal --force 
-libtoolize --copy --force
-/usr/bin/autoconf --force
-/usr/bin/autoheader --force
-automake --add-missing --copy --force-missing
+autoreconf -v -f --install || exit 1
 
 ./configure \
 	CFLAGS="-O2 -Wall" \
