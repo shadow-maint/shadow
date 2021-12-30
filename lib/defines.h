9e788adf56f4e9952d7223eb14a497645a03f842
Alejandro Colomar <alx.manpages@gmail.com>
2021-12-30 14:25:49 +0100

    Remove AC_HEADER_SYS_WAIT
    GNU autoconf documentation marks this macro as obsolescent, as
    current systems are compatible with POSIX.
    
    Simplify code to unconditionally include <sys/wait.h>, and don't
    redefine WIFEXITSTATUS() and WIFEXITED(), since they are mandated
    by POSIX.
    
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- lib/defines.h
+++ lib/defines.h
@@ -68,15 +68,7 @@ extern char * textdomain (const char * domainname);
 
 #include <sys/stat.h>
 #include <sys/types.h>
-#if HAVE_SYS_WAIT_H
-# include <sys/wait.h>
-#endif
-#ifndef WEXITSTATUS
-# define WEXITSTATUS(stat_val) ((unsigned)(stat_val) >> 8)
-#endif
-#ifndef WIFEXITED
-# define WIFEXITED(stat_val) (((stat_val) & 255) == 0)
-#endif
+#include <sys/wait.h>
 
 #if HAVE_UNISTD_H
 # include <unistd.h>
