0c7ded471fdd2a130edfb265279663c68cfd2a3c
Iker Pedrosa <ipedrosa@redhat.com>
2022-05-10 15:26:15 +0200

    Add header guards
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

--- lib/shadowlog_internal.h
+++ lib/shadowlog_internal.h
@@ -1,2 +1,7 @@
+#ifndef _SHADOWLOG_INTERNAL_H
+#define _SHADOWLOG_INTERNAL_H
+
 extern const char *shadow_progname; /* Program name showed in error messages */
 extern FILE *shadow_logfd;  /* file descripter to which error messages are printed */
+
+#endif /* _SHADOWLOG_INTERNAL_H */
