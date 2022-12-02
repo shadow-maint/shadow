87c123620d21d92a6037c8386369f703c32b2518
Alejandro Colomar <alx@kernel.org>
2022-12-02 22:41:48 +0100

    Assume getgrnam_r(3) exists
    It is required by POSIX.1-2001.
    
    Cc: Christian Göttsche <cgzones@googlemail.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- libmisc/xgetgrnam.c
+++ libmisc/xgetgrnam.c
@@ -35,7 +35,7 @@
 #define ARG_TYPE	const char *
 #define ARG_NAME	name
 #define DUP_FUNCTION	__gr_dup
-#define HAVE_FUNCTION_R (defined HAVE_GETGRNAM_R)
+#define HAVE_FUNCTION_R 1
 
 #include "xgetXXbyYY.c"
 
