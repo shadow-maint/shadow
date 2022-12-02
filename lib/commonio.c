e71c23586a8921b6c5feab5bffa5d3d11e546a74
Alejandro Colomar <alx@kernel.org>
2022-12-02 22:31:38 +0100

    Assume fchown(2) exists
    It is required by POSIX.1-2001.
    
    Cc: Christian Göttsche <cgzones@googlemail.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/commonio.c
+++ lib/commonio.c
@@ -251,16 +251,9 @@ static /*@null@*/ /*@dependent@*/FILE *fopen_set_perms (
 		return NULL;
 	}
 
-#ifdef HAVE_FCHOWN
 	if (fchown (fileno (fp), sb->st_uid, sb->st_gid) != 0) {
 		goto fail;
 	}
-#else				/* !HAVE_FCHOWN */
-	if (chown (name, sb->st_mode) != 0) {
-		goto fail;
-	}
-#endif				/* !HAVE_FCHOWN */
-
 	if (fchmod (fileno (fp), sb->st_mode & 0664) != 0) {
 		goto fail;
 	}
