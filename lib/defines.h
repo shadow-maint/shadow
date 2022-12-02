aa5f77daa4e65f7332ce40307aa6545c4701c47e
Alejandro Colomar <alx@kernel.org>
2022-12-02 16:51:05 +0100

    Assume <stdbool.h> exists
    ISO C99 requires <stdbool.h>.
    
    Many files in the project already include <stdbool.h> unconditionally,
    so it's reasonable to assume that it is always available.
    
    Link: <https://github.com/shadow-maint/shadow/pull/600>
    Cc: Christian Göttsche <cgzones@googlemail.com>
    Cc: Iker Pedrosa <ipedrosa@redhat.com>
    Cc: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/defines.h
+++ lib/defines.h
@@ -6,21 +6,7 @@
 
 #include "config.h"
 
-#if HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# if ! HAVE__BOOL
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  else
-typedef unsigned char _Bool;
-#  endif
-# endif
-# define bool _Bool
-# define false (0)
-# define true  (1)
-# define __bool_true_false_are_defined 1
-#endif
+#include <stdbool.h>
 
 /* Take care of NLS matters.  */
 #ifdef S_SPLINT_S
