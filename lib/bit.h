0712b236c3bc93987ce64f7631f5a9fa4b042f4f
Alejandro Colomar <alx@kernel.org>
2023-01-30 12:43:34 +0100

    Add bit manipulation functions
    We do need the unoptimized version of csrand_uniform() for high values
    of `n`, since the optimized version depends on having __int128, and it's
    not available on several platforms, including ARMv7, IA32, and MK68k.
    
    This reverts commit 848f53c1d3c1362c86d3baab6906e1e4419d2634; however,
    I applied some tweaks to the reverted commit.
    
    Reported-by: Adam Sampson <ats@offog.org>
    Cc: Iker Pedrosa <ipedrosa@redhat.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/bit.h
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText:  2022 - 2023, Alejandro Colomar <alx@kernel.org>
+ *
+ * SPDX-License-Identifier:  BSD-3-Clause
+ */
+
+
+#ifndef SHADOW_INCLUDE_LIB_BIT_H_
+#define SHADOW_INCLUDE_LIB_BIT_H_
+
+
+#include <config.h>
+
+#include <limits.h>
+
+
+inline unsigned long bit_ceilul(unsigned long x);
+inline unsigned long bit_ceil_wrapul(unsigned long x);
+inline int leading_zerosul(unsigned long x);
+
+
+/* stdc_bit_ceilul(3) */
+inline unsigned long
+bit_ceilul(unsigned long x)
+{
+	return 1 + (ULONG_MAX >> leading_zerosul(x));
+}
+
+
+/* stdc_bit_ceilul(3), but wrap instead of having Undefined Behavior */
+inline unsigned long
+bit_ceil_wrapul(unsigned long x)
+{
+	if (x == 0)
+		return 0;
+
+	return bit_ceil_wrapul(x);
+}
+
+/* stdc_leading_zerosul(3) */
+inline int
+leading_zerosul(unsigned long x)
+{
+	return (x == 0) ? ULONG_WIDTH : __builtin_clz(x);
+}
+
+
+#endif  // include guard
