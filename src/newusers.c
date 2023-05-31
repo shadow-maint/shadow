shadow-maint/shadow

commit b422e3c31691412f0a5404d09f7b328477e23c48

Check if crypt_method null before dereferencing
Make sure crypto_method set before sha-rounds. Only affects newusers.

src/newusers.c
@@ -662,6 +662,13 @@ static void process_flags (int argc, char **argv)
 		case 's':
 			sflg = true;
                         bad_s = 0;
+
+			if (!crypt_method){
+				fprintf(stderr,
+						_("%s: Provide '--crypt-method'\n"),
+						Prog);
+				usage (EXIT_FAILURE);
+			}
 #if defined(USE_SHA_CRYPT)
 			if (  (   ((0 == strcmp (crypt_method, "SHA256")) || (0 == strcmp (crypt_method, "SHA512")))
 			       && (0 == getlong(optarg, &sha_rounds)))) {

commit 861c92195658ed1659227ba6cf13ff15d1309436

newusers: Improve error message
Fixes: b422e3c31691: Check if crypt_method null before dereferencing

Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

src/newusers.c
@@ -665,7 +665,7 @@ static void process_flags (int argc, char **argv)
 
 			if (!crypt_method){
 				fprintf(stderr,
-						_("%s: Provide '--crypt-method'\n"),
+						_("%s: Provide '--crypt-method' before number of rounds\n"),
 						Prog);
 				usage (EXIT_FAILURE);
 			}