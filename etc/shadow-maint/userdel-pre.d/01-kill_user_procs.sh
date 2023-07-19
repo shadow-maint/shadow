7bced397c9fd66965753e5fc0fd3dfa535ca1c9b
xiongshenglan <xiongshenglan@huawei.com>
2023-07-19 15:13:06 +0800

    shadow userdel: add the adaptation to the busybox ps in 01-kill_user_procs.sh
    In some embedded systems, users only use the ps
    provided by the busybox. But the ps provided by
    the busybox does not support the -eo option by
    default. As a result, an error is reported when
    the userdel is used. So add a judgment on ps.
    If there is no ps -eo, traverse the process directly.
    
    The error information is as follows:
     # userdel xsl
    ps: invalid option -- 'e'
    
    Signed-off-by: xiongshenglan <xiongshenglan@huawei.com>

--- etc/shadow-maint/userdel-pre.d/01-kill_user_procs.sh
+++ etc/shadow-maint/userdel-pre.d/01-kill_user_procs.sh
@@ -4,13 +4,16 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # Check user exists, and if so, send sigkill to processes that the user owns
 
-RUNNING=`ps -eo user | grep -Fx "$SUBJECT" | wc -l`
-
-# if the user does not exist, RUNNING will be 0
-
-if [ "${RUNNING}x" = "0x" ]; then
-  exit 0
+ps -eo user >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    RUNNING=`ps -eo user | grep -Fx "$SUBJECT" | wc -l`
+    # if the user does not exist, RUNNING will be 0
+    if [ "${RUNNING}x" = "0x" ]; then
+        exit 0
+    fi
 fi
+
+# If there is no ps -eo, traverse the process directly.
 
 ls -1 /proc | while IFS= read -r PROC; do
   echo "$PROC" | grep -E '^[0-9]+$' >/dev/null
