bc82d3a27b1595a2c252294f1acab0107cfdcf71
Alejandro Colomar <alx@kernel.org>
2023-09-02 17:31:15 +0200

    autogen.sh: Prepare CFLAGS before ./configure
    Reviewed-by: Iker Pedrosa <ipedrosa@redhat.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,12 @@
 
 autoreconf -v -f --install "$(dirname "$0")" || exit 1
 
+CFLAGS="-O2"
+CFLAGS="$CFLAGS -Wall"
+CFLAGS="$CFLAGS -Werror=implicit-function-declaration"
+
 "$(dirname "$0")"/configure \
-	CFLAGS="-O2 -Wall -Werror=implicit-function-declaration" \
+	CFLAGS="$CFLAGS" \
 	--enable-lastlog \
 	--enable-man \
 	--enable-maintainer-mode \
