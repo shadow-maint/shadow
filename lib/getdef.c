fa68441bc4be8edeaad8d104344e987b31ed44e6
Serge Hallyn <serge@hallyn.com>
2023-10-04 10:38:48 -0500

    Improve the login.defs unknown item error message
    Closes #746
    
    Only print the 'unknown item' message to syslog if we are
    actually parsing a login.defs.  Prefix it with "shadow:" to make
    it clear in syslog where it came from.
    
    Also add the source filename to the console message.  I'm not
    quite clear on the econf API, so not sure whether in that path we
    will end up actually having the path, or printing ''.
    
    Signed-off-by: Serge Hallyn <serge@hallyn.com>

--- lib/getdef.c
+++ lib/getdef.c
@@ -363,7 +363,7 @@ unsigned long getdef_ulong (const char *item, unsigned long dflt)
  * (also used when loading the initial defaults)
  */
 
-int putdef_str (const char *name, const char *value)
+int putdef_str (const char *name, const char *value, const char *srcfile)
 {
 	struct itemdef *d;
 	char *cp;
@@ -378,6 +378,8 @@ int putdef_str (const char *name, const char *value)
 	 */
 	d = def_find (name);
 	if (NULL == d) {
+		if (NULL != srcfile)
+			SYSLOG ((LOG_CRIT, "shadow: unknown configuration item '%s' in '%s'", name, srcfile));
 		return -1;
 	}
 
@@ -431,7 +433,6 @@ static /*@observer@*/ /*@null@*/struct itemdef *def_find (const char *name)
 	fprintf (shadow_logfd,
 	         _("configuration error - unknown item '%s' (notify administrator)\n"),
 	         name);
-	SYSLOG ((LOG_CRIT, "unknown configuration item `%s'", name));
 
 out:
 	return NULL;
@@ -512,7 +513,7 @@ static void def_load (void)
 		 * The error was already reported to the user and to
 		 * syslog. The tools will just use their default values.
 		 */
-		(void)putdef_str (keys[i], value);
+		(void)putdef_str (keys[i], value, econf_getPath(defs_file));
 
 		free(value);
 	}
@@ -585,7 +586,7 @@ static void def_load (void)
 		 * The error was already reported to the user and to
 		 * syslog. The tools will just use their default values.
 		 */
-		(void)putdef_str (name, value);
+		(void)putdef_str (name, value, def_fname);
 	}
 
 	if (ferror (fp) != 0) {
