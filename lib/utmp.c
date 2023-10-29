shadow-maint/shadow

commit 5178f8c5afb612f6ddf5363823547e080e7f546b

utmp: call prepare_utmp() even if utent is NULL
update_utmp() should also return 0 when success.

Fixes: 1f368e1c1838de9d476a36897d7c53394569de08 ("utmp: update
`update_utmp()")
Resolves: https://github.com/shadow-maint/shadow/issues/805

Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

lib/utmp.c
@@ -370,17 +370,16 @@ int update_utmp (const char *user,
 	struct utmp *utent, *ut;
 
 	utent = get_current_utmp ();
-	if (utent == NULL) {
-		return -1;
-	}
-
 	ut = prepare_utmp  (user, tty, host, utent);
 
 	(void) setutmp  (ut);	/* make entry in the utmp & wtmp files */
-	free (utent);
+
+	if (utent != NULL) {
+		free (utent);
+	}
 	free (ut);
 
-	return 1;
+	return 0;
 }
 
 void record_failure(const char *failent_user,

commit 3a084a4b66609c906559c632b33c73ba9f7fc9ce

lib/utmp.c: Don't check for NULL before free(3)
free(NULL) is valid; there's no need to check for NULL.  Simplify.

Fixes: 5178f8c5afb6 ("utmp: call prepare_utmp() even if utent is NULL")
Signed-off-by: Alejandro Colomar <alx@kernel.org>

lib/utmp.c
@@ -374,10 +374,8 @@ int update_utmp (const char *user,
 
 	(void) setutmp  (ut);	/* make entry in the utmp & wtmp files */
 
-	if (utent != NULL) {
-		free (utent);
-	}
-	free (ut);
+	free(utent);
+	free(ut);
 
 	return 0;
 }