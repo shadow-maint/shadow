1c50a44db6bb02984fabbb482fea70ef2ca08fb2
Alejandro Colomar <alx@kernel.org>
2023-11-16 15:56:25 +0100

    lib/date_to_str.c: strftime(3) leaves the buffer undefined on failure
    strftime(3) makes no guarantees about the contents of the buffer if the
    formatted string wouldn't fit in the buffer.  It simply returns 0, and
    it's the programmer's responsibility to do the right thing after that.
    
    Let's write the string "future" if there's an error, similar to what we
    do with gmtime(3)'s errors.
    
    Also, `buf[size - 1] = '\0';` didn't make sense.  If the copy fits,
    strftime(3) guarantees to terminate with NUL.  If it doesn't, the entire
    contents of buf are undefined, so adding a NUL at the end of the buffer
    would be dangerous: the string could contain anything, such as
    "gimme root access now".  Remove that, now that we set the string to
    "future", as with gmtime(3) errors.  This setting to '\0' comes from the
    times when we used strncpy(3) in the implementation, and should have
    been removed when I changed it to use strlcpy(3); however, I didn't
    check we didn't need it anymore.
    
    Cc: Serge Hallyn <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/date_to_str.c
+++ lib/date_to_str.c
@@ -1,30 +1,8 @@
 /*
- * Copyright (c) 2021, Alejandro Colomar <alx.manpages@gmail.com>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the copyright holders or contributors may not be used to
- *    endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
- * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-FileCopyrightText: 2021-2023, Alejandro Colomar <alx@kernel.org>
+ * SPDX-License-Identifier: BSD-3-Clause
  */
+
 
 #include <config.h>
 
@@ -35,6 +13,7 @@
 
 #include "strtcpy.h"
 #include "prototypes.h"
+
 
 void
 date_to_str(size_t size, char buf[size], long date)
@@ -54,6 +33,6 @@ date_to_str(size_t size, char buf[size], long date)
 		return;
 	}
 
-	(void) strftime(buf, size, "%Y-%m-%d", tm);
-	buf[size - 1] = '\0';
+	if (strftime(buf, size, "%Y-%m-%d", tm) == 0)
+		(void) strtcpy(buf, "future", size);
 }
