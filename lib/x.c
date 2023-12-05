58f452c9828119d148fb7bb9d8b1296e61394543
Alejandro Colomar <alx@kernel.org>
2023-12-05 13:46:23 +0100

    lib/: X(): Add macro to exit() on error
    Writing an x*() variant of several functions is unnecessary.  It's
    simpler to write a generic X() macro that can be chained with any other
    calls.
    
    For example:
    
    	-  X(malloc(...)) instead of xmalloc(...);
    	-  X(strdup(...)) instead of xstrdup(...);
    
    If an error is detected, log an error, and exit(13).  About why 13, I
    don't really know.  It's just what was used previously in xmalloc().
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/x.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2023-2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include "config.h"
+
+#include "x.h"
+
+
+extern inline void *x_(void *p);
