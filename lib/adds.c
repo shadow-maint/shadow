89e5a3296662cdaf930e6ea372afbab812384f10
Alejandro Colomar <alx@kernel.org>
2023-12-20 21:43:03 +0100

    lib/adds.[ch]: Add addsl() and addsl3()
    These functions add 2 or 3 longs, saturating to LONG_{MIN,MAX} instead
    of overflowing.
    
    Cc: Tobias Stoeckmann <tobias@stoeckmann.org>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/adds.c
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "adds.h"
+
+
+extern inline long addsl(long a, long b);
+extern inline long addsl3(long a, long b, long c);
+
+extern inline int cmpl(const void *p1, const void *p2);
