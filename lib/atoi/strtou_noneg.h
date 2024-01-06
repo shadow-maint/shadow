f2b240595b1c0f10f3a73cceb3bc4f6226cb8818
Alejandro Colomar <alx@kernel.org>
2024-01-06 19:46:20 +0100

    lib/atoi/strtou_noneg.[ch]: Add strtou_noneg()
    It's like strtou_(), but rejects negative input, instead of silently
    converting it to unsigned.
    
    Link: <https://softwareengineering.stackexchange.com/a/449060/332848>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/atoi/strtou_noneg.h
+++ lib/atoi/strtou_noneg.h
@@ -9,10 +9,16 @@
 #include <config.h>
 
 #include <errno.h>
+#include <stdint.h>
 #include <stdlib.h>
 
+#include "atoi/strtoi.h"
 #include "attr.h"
 
+
+ATTR_STRING(1) ATTR_ACCESS(write_only, 2) ATTR_ACCESS(write_only, 6)
+inline uintmax_t strtou_noneg(const char *s, char **restrict endp,
+    int base, uintmax_t min, uintmax_t max, int *restrict status);
 
 ATTR_STRING(1) ATTR_ACCESS(write_only, 2)
 inline unsigned long strtoul_noneg(const char *s,
@@ -20,6 +26,21 @@ inline unsigned long strtoul_noneg(const char *s,
 ATTR_STRING(1) ATTR_ACCESS(write_only, 2)
 inline unsigned long long strtoull_noneg(const char *s,
     char **restrict endp, int base);
+
+
+inline uintmax_t
+strtou_noneg(const char *s, char **restrict endp, int base,
+    uintmax_t min, uintmax_t max, int *restrict status)
+{
+	int  st;
+
+	if (status == NULL)
+		status = &st;
+	if (strtoi_(s, endp, base, 0, 1, status) == 0 && *status == ERANGE)
+		return min;
+
+	return strtou_(s, endp, base, min, max, status);
+}
 
 
 inline unsigned long
