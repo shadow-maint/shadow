2e56af190222f2f3c3d26a58f2f085f55c98dd3c
Alejandro Colomar <alx@kernel.org>
2024-01-06 02:17:38 +0100

    lib/, tests/: addsl(): Add addsl(), a variadic macro
    Add a variadic macro addsl() that accepts an arbitrary number of
    addends, instead of having specific versions like addsl2() or addsl3().
    
    It is internally implemented by the addslN() function, which itself
    calls addsl2().  addsl3() is now obsolete and thus removed.
    
    Code should just call addsl().
    
    Link: <https://github.com/shadow-maint/shadow/pull/882#discussion_r1437155212>
    Cc: Serge Hallyn <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/adds.c
+++ lib/adds.c
@@ -6,8 +6,10 @@
 
 #include "adds.h"
 
+#include <stddef.h>
+
 
 extern inline long addsl2(long a, long b);
-extern inline long addsl3(long a, long b, long c);
+extern inline long addslN(size_t n, long addend[n]);
 
 extern inline int cmpl(const void *p1, const void *p2);
