da60007e2683a514b100625a6b374c4c427b74c0
Alejandro Colomar <alx@kernel.org>
2024-01-09 20:54:38 +0100

    src/check_subid_range.c: Call get_uid() instead of getl()
    This value represents a uid.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/check_subid_range.c
+++ src/check_subid_range.c
@@ -14,6 +14,7 @@
 #include <fcntl.h>
 
 #include "atoi/getlong.h"
+#include "atoi/getnum.h"
 #include "defines.h"
 #include "prototypes.h"
 #include "subordinateio.h"
@@ -24,9 +25,11 @@ const char *Prog;
 
 int main(int argc, char **argv)
 {
-	char *owner;
-	unsigned long start, count;
-	bool check_uids;
+	bool           check_uids;
+	char           *owner;
+	uid_t          start;
+	unsigned long  count;
+
 	Prog = Basename (argv[0]);
 	log_set_progname(Prog);
 	log_set_logfd(stderr);
@@ -37,7 +40,7 @@ int main(int argc, char **argv)
 	owner = argv[1];
 	check_uids = argv[2][0] == 'u';
 	errno = 0;
-	if (getul(argv[3], &start) == -1)
+	if (get_uid(argv[3], &start) == -1)
 		exit(1);
 	if (getul(argv[4], &count) == -1)
 		exit(1);
