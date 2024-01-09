c8bda9191115b1257fd7ae3a8825106b359b4a91
Alejandro Colomar <alx@kernel.org>
2024-01-09 15:48:55 +0100

    lib/: Don't open-code get_gid()
    These functions were open-coding get_gid().  Use the actual function.
    
    Reviewed-by: "Serge E. Hallyn" <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/getgr_nam_gid.c
+++ lib/getgr_nam_gid.c
@@ -1,11 +1,10 @@
-/*
- * SPDX-FileCopyrightText: 1991 - 1994, Julianne Frances Haugh
- * SPDX-FileCopyrightText: 1996 - 2000, Marek Michałkiewicz
- * SPDX-FileCopyrightText: 2000 - 2006, Tomasz Kłoczko
- * SPDX-FileCopyrightText: 2007 - 2009, Nicolas François
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+// SPDX-FileCopyrightText: 1991-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-2000, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2000-2006, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2007-2009, Nicolas François
+// SPDX-FileCopyrightText: 2023-2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
 
 #include <config.h>
 
@@ -14,30 +13,27 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <grp.h>
+#include <sys/types.h>
+
+#include "atoi/getnum.h"
 #include "prototypes.h"
+
 
 /*
  * getgr_nam_gid - Return a pointer to the group specified by a string.
  * The string may be a valid GID or a valid groupname.
  * If the group does not exist on the system, NULL is returned.
  */
-extern /*@only@*//*@null@*/struct group *getgr_nam_gid (/*@null@*/const char *grname)
+extern /*@only@*//*@null@*/struct group *
+getgr_nam_gid(/*@null@*/const char *grname)
 {
-	char       *end;
-	long long  gid;
+	gid_t  gid;
 
-	if (NULL == grname) {
+	if (NULL == grname)
 		return NULL;
-	}
 
-	errno = 0;
-	gid = strtoll(grname, &end, 10);
-	if (   ('\0' != *grname)
-	    && ('\0' == *end)
-	    && (0 == errno)
-	    && (/*@+longintegral@*/gid == (gid_t)gid)/*@=longintegral@*/) {
-		return xgetgrgid (gid);
-	}
-	return xgetgrnam (grname);
+	if (get_gid(grname, &gid) == 0)
+		return xgetgrgid(gid);
+	return xgetgrnam(grname);
 }
 
