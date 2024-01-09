71e28359d12491727b2e94c71d2e1e1682d45a02
Alejandro Colomar <alx@kernel.org>
2024-01-09 15:00:15 +0100

    lib/atoi/strtou_noneg.[ch], tests/: strtoul_noneg(): Remove unused function
    All call sites have been replaced by functions from "atoi/a2i.h" and
    "atoi/str2i.h" recently.
    
    Reviewed-by: Iker Pedrosa <ipedrosa@redhat.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/atoi/strtou_noneg.h
+++ lib/atoi/strtou_noneg.h
@@ -9,8 +9,8 @@
 #include <config.h>
 
 #include <errno.h>
+#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 #include "atoi/strtoi.h"
 #include "attr.h"
@@ -19,10 +19,6 @@
 ATTR_STRING(1) ATTR_ACCESS(write_only, 2) ATTR_ACCESS(write_only, 6)
 inline uintmax_t strtou_noneg(const char *s, char **restrict endp,
     int base, uintmax_t min, uintmax_t max, int *restrict status);
-
-ATTR_STRING(1) ATTR_ACCESS(write_only, 2)
-inline unsigned long strtoul_noneg(const char *s,
-    char **restrict endp, int base);
 
 
 inline uintmax_t
@@ -37,17 +33,6 @@ strtou_noneg(const char *s, char **restrict endp, int base,
 		return min;
 
 	return strtou_(s, endp, base, min, max, status);
-}
-
-
-inline unsigned long
-strtoul_noneg(const char *s, char **restrict endp, int base)
-{
-	if (strtol(s, endp, base) < 0) {
-		errno = ERANGE;
-		return 0;
-	}
-	return strtoul(s, endp, base);
 }
 
 
