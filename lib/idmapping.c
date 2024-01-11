458529a6d716e46c04626d2e5e6eb52231d92e7c
Alejandro Colomar <alx@kernel.org>
2024-01-11 12:15:11 +0100

    lib/idmapping.c: get_map_ranges(): Move range check to a2ul() call
    Link: <https://github.com/shadow-maint/shadow/commit/ff2baed5dbf81e8967b805889f565fedb48600df#r136635300>
    Cc: Serge Hallyn <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/idmapping.c
+++ lib/idmapping.c
@@ -68,7 +68,10 @@ struct map_range *get_map_ranges(int ranges, int argc, char **argv)
 			free(mappings);
 			return NULL;
 		}
-		if (a2ul(&m->count, argv[argidx + 2], NULL, 0, 0, UINT_MAX) == -1) {
+		if (a2ul(&m->count, argv[argidx + 2], NULL, 0, 0,
+		         MIN(UINT_MAX - m->lower, UINT_MAX - m->upper))
+		    == -1)
+		{
 			if (errno == ERANGE) {
 				fprintf(log_get_logfd(), _( "%s: subuid overflow detected.\n"), log_get_progname());
 				exit(EXIT_FAILURE);
@@ -77,10 +80,6 @@ struct map_range *get_map_ranges(int ranges, int argc, char **argv)
 			return NULL;
 		}
 		if (ULONG_MAX - m->upper <= m->count || ULONG_MAX - m->lower <= m->count) {
-			fprintf(log_get_logfd(), _( "%s: subuid overflow detected.\n"), log_get_progname());
-			exit(EXIT_FAILURE);
-		}
-		if (m->lower + m->count > UINT_MAX || m->upper + m->count > UINT_MAX) {
 			fprintf(log_get_logfd(), _( "%s: subuid overflow detected.\n"), log_get_progname());
 			exit(EXIT_FAILURE);
 		}
