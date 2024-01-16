01e906fe2ef2c4068293005ec66318b02272b0ca
Alejandro Colomar <alx@kernel.org>
2024-01-16 21:48:22 +0100

    lib/atoi/: a2*(), str2*(): Add variants for other types
    And type-generic macros that wrap them: a2i(), str2i()
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/atoi/a2i.c
+++ lib/atoi/a2i.c
@@ -7,7 +7,20 @@
 #include "atoi/a2i.h"
 
 
+extern inline int a2sh(short *restrict n, const char *s,
+    char **restrict endp, int base, short min, short max);
+extern inline int a2si(int *restrict n, const char *s,
+    char **restrict endp, int base, int min, int max);
 extern inline int a2sl(long *restrict n, const char *s,
     char **restrict endp, int base, long min, long max);
+extern inline int a2sll(long long *restrict n, const char *s,
+    char **restrict endp, int base, long long min, long long max);
+extern inline int a2uh(unsigned short *restrict n, const char *s,
+    char **restrict endp, int base, unsigned short min, unsigned short max);
+extern inline int a2ui(unsigned int *restrict n, const char *s,
+    char **restrict endp, int base, unsigned int min, unsigned int max);
 extern inline int a2ul(unsigned long *restrict n, const char *s,
     char **restrict endp, int base, unsigned long min, unsigned long max);
+extern inline int a2ull(unsigned long long *restrict n, const char *s,
+    char **restrict endp, int base, unsigned long long min,
+    unsigned long long max);
--- lib/atoi/str2i.c
+++ lib/atoi/str2i.c
@@ -8,5 +8,11 @@
 #include "atoi/str2i.h"
 
 
+extern inline int str2sh(short *restrict n, const char *restrict s);
+extern inline int str2si(int *restrict n, const char *restrict s);
 extern inline int str2sl(long *restrict n, const char *restrict s);
+extern inline int str2sll(long long *restrict n, const char *restrict s);
+extern inline int str2uh(unsigned short *restrict n, const char *restrict s);
+extern inline int str2ui(unsigned int *restrict n, const char *restrict s);
 extern inline int str2ul(unsigned long *restrict n, const char *restrict s);
+extern inline int str2ull(unsigned long long *restrict n, const char *restrict s);
