5214710432f531ffde8763e44ce9355bbdb58768
Alejandro Colomar <alx@kernel.org>
2024-01-17 16:52:12 +0100

    src/sulogin.c: pw_entry(): Don't else after return
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/sulogin.c
+++ src/sulogin.c
@@ -197,23 +197,23 @@ pw_entry(const char *name, struct passwd *pwent)
 
 	struct spwd *spwd;
 
-	if (!(passwd = getpwnam (name))) { /* local, no need for xgetpwnam */
+	if (!(passwd = getpwnam(name))) {  /* local, no need for xgetpwnam */
 		pwent->pw_name = NULL;
 		return;
-	} else {
-		pwent->pw_name = xstrdup (passwd->pw_name);
-		pwent->pw_uid = passwd->pw_uid;
-		pwent->pw_gid = passwd->pw_gid;
-		pwent->pw_gecos = xstrdup (passwd->pw_gecos);
-		pwent->pw_dir = xstrdup (passwd->pw_dir);
-		pwent->pw_shell = xstrdup (passwd->pw_shell);
-#if !defined(AUTOSHADOW)
-		/* local, no need for xgetspnam */
-		if ((spwd = getspnam (name))) {
-			pwent->pw_passwd = xstrdup (spwd->sp_pwdp);
-			return;
-		}
-#endif
-		pwent->pw_passwd = xstrdup (passwd->pw_passwd);
 	}
+
+	pwent->pw_name = xstrdup(passwd->pw_name);
+	pwent->pw_uid = passwd->pw_uid;
+	pwent->pw_gid = passwd->pw_gid;
+	pwent->pw_gecos = xstrdup(passwd->pw_gecos);
+	pwent->pw_dir = xstrdup(passwd->pw_dir);
+	pwent->pw_shell = xstrdup(passwd->pw_shell);
+#if !defined(AUTOSHADOW)
+	/* local, no need for xgetspnam */
+	if ((spwd = getspnam(name))) {
+		pwent->pw_passwd = xstrdup(spwd->sp_pwdp);
+		return;
+	}
+#endif
+	pwent->pw_passwd = xstrdup(passwd->pw_passwd);
 }
