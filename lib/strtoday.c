781724c7c295a1749101e962bafa6c77ffcc9221
Alejandro Colomar <alx@kernel.org>
2024-02-09 01:39:18 +0100

    lib/strtoday.c: strtoday(): Reimplement in terms of getdate(3)+mktime(3)
    This removes the only use of get_date(), in <lib/getdate.y> which is a
    monster.  Now we can remove all of that code.  Let's do that in the next
    commit.
    
    Remove comment regarding get_date().  We still need the NULL check,
    though, since getdate(3) doesn't accept a null pointer.
    
    Cc: Iker Pedrosa <ipedrosa@redhat.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/strtoday.c
+++ lib/strtoday.c
@@ -1,49 +1,30 @@
-/*
- * SPDX-FileCopyrightText: 1991 - 1994, Julianne Frances Haugh
- * SPDX-FileCopyrightText: 1996 - 1999, Marek Michałkiewicz
- * SPDX-FileCopyrightText: 2003 - 2005, Tomasz Kłoczko
- * SPDX-FileCopyrightText: 2008       , Nicolas François
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+// SPDX-FileCopyrightText: 1991-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-1999, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2003-2005, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2008, Nicolas François
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include <config.h>
 
 #include <ctype.h>
+#include <time.h>
 
 #ident "$Id$"
 
 #include "prototypes.h"
-#include "getdate.h"
 
-/*
- * strtoday() now uses get_date() (borrowed from GNU shellutils)
- * which can handle many date formats, for example:
- *	1970-09-17	# ISO 8601.
- *	70-9-17		# This century assumed by default.
- *	70-09-17	# Leading zeros are ignored.
- *	9/17/72		# Common U.S. writing.
- *	24 September 1972
- *	24 Sept 72	# September has a special abbreviation.
- *	24 Sep 72	# Three-letter abbreviations always allowed.
- *	Sep 24, 1972
- *	24-sep-72
- *	24sep72
- */
-long strtoday (const char *str)
+
+long
+strtoday(const char *str)
 {
-	time_t t;
-	bool isnum = true;
-	const char *s = str;
+	bool        isnum = true;
+	time_t      t;
+	struct tm   *tp;
+	const char  *s = str;
 
-	/*
-	 * get_date() interprets an empty string as the current date,
-	 * which is not what we expect, unless you're a BOFH :-).
-	 * (useradd sets sp_expire = current date for new lusers)
-	 */
-	if ((NULL == str) || ('\0' == *str)) {
+	if ((NULL == str) || ('\0' == *str))
 		return -1;
-	}
 
 	/* If a numerical value is provided, this is already a number of
 	 * days since EPOCH.
@@ -68,9 +49,13 @@ long strtoday (const char *str)
 		return retdate;
 	}
 
-	t = get_date(str, NULL);
-	if ((time_t) - 1 == t) {
+	tp = getdate(str);
+	if (tp == NULL)
 		return -2;
-	}
+
+	t = mktime(tp);
+	if ((time_t) -1 == t)
+		return -2;
+
 	return t / DAY;
 }
