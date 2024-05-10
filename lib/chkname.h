53067b087c6615d39c99382153128b882496a1c7
Alejandro Colomar <alx@kernel.org>
2024-05-11 01:41:52 +0200

    lib/chkname.[ch]: Fix includes
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/chkname.h
+++ lib/chkname.h
@@ -11,6 +11,7 @@
 #ifndef _CHKNAME_H_
 #define _CHKNAME_H_
 
+
 /*
  * is_valid_user_name(), is_valid_group_name() - check the new user/group
  * name for validity;
@@ -19,7 +20,11 @@
  *   false - bad name
  */
 
-#include "defines.h"
+
+#include <config.h>
+
+#include <stdbool.h>
+
 
 extern bool is_valid_user_name (const char *name);
 extern bool is_valid_group_name (const char *name);
