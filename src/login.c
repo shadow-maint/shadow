shadow-maint/shadow

commit 3b7cc053872cf4ce77fd74dc037f43f34e951e83

lib: replace `USER_NAME_MAX_LENGTH` macro
Replace it by `sysconf(_SC_LOGIN_NAME_MAX)`, which is the maximum
username length supported by the kernel.

Resolves: https://github.com/shadow-maint/shadow/issues/674

Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

src/login.c
@@ -572,10 +572,11 @@ int main (int argc, char **argv)
 	}
 #ifdef RLOGIN
 	if (rflg) {
+		size_t max_size = sysconf(_SC_LOGIN_NAME_MAX);
 		assert (NULL == username);
-		username = XMALLOC(USER_NAME_MAX_LENGTH + 1, char);
-		username[USER_NAME_MAX_LENGTH] = '\0';
-		if (do_rlogin (hostname, username, USER_NAME_MAX_LENGTH, term, sizeof term)) {
+		username = XMALLOC(max_size + 1, char);
+		username[max_size] = '\0';
+		if (do_rlogin (hostname, username, max_size, term, sizeof term)) {
 			preauth_flag = true;
 		} else {
 			free (username);
@@ -884,14 +885,15 @@ int main (int argc, char **argv)
 
 		failed = false;	/* haven't failed authentication yet */
 		if (NULL == username) {	/* need to get a login id */
+			size_t max_size = sysconf(_SC_LOGIN_NAME_MAX);
 			if (subroot) {
 				closelog ();
 				exit (1);
 			}
 			preauth_flag = false;
-			username = XMALLOC(USER_NAME_MAX_LENGTH + 1, char);
-			username[USER_NAME_MAX_LENGTH] = '\0';
-			login_prompt (username, USER_NAME_MAX_LENGTH);
+			username = XMALLOC(max_size + 1, char);
+			username[max_size] = '\0';
+			login_prompt (username, max_size);
 
 			if ('\0' == username[0]) {
 				/* Prompt for a new login */

lib/defines.h
@@ -232,10 +232,6 @@ static inline void memzero(void *ptr, size_t size)
 # define format_attr(type, index, check)
 #endif
 
-/* Maximum length of usernames */
-#include <utmp.h>
-#define USER_NAME_MAX_LENGTH (sizeof (((struct utmp *)NULL)->ut_user))
-
 /* Maximum length of passwd entry */
 #define PASSWD_ENTRY_MAX_LENGTH 32768
 

libmisc/chkname.c
@@ -75,10 +75,9 @@ static bool is_valid_name (const char *name)
 bool is_valid_user_name (const char *name)
 {
 	/*
-	 * User names are limited by whatever utmp can
-	 * handle.
+	 * User names length are limited by the kernel
 	 */
-	if (strlen (name) > USER_NAME_MAX_LENGTH) {
+	if (strlen (name) > sysconf(_SC_LOGIN_NAME_MAX)) {
 		return false;
 	}
 

commit 51709012e953690b0bc44b1a4eebf9876e39a61f

src/login.c: main(): Use login_name_max_size()
Instead of raw sysconf(_SC_LOGIN_NAME_MAX) calls, which was being used
without error handling.

Fixes: 3b7cc053872c ("lib: replace `USER_NAME_MAX_LENGTH` macro")
Signed-off-by: Alejandro Colomar <alx@kernel.org>

src/login.c
@@ -27,6 +27,7 @@
 
 #include "alloc.h"
 #include "attr.h"
+#include "chkname.h"
 #include "defines.h"
 #include "faillog.h"
 #include "failure.h"
@@ -573,8 +574,9 @@ int main (int argc, char **argv)
 	}
 #ifdef RLOGIN
 	if (rflg) {
-		size_t  max_size = sysconf(_SC_LOGIN_NAME_MAX);
+		size_t  max_size;
 
+		max_size = login_name_max_size();
 		assert (NULL == username);
 		username = XMALLOC(max_size, char);
 		username[max_size - 1] = '\0';
@@ -882,8 +884,9 @@ int main (int argc, char **argv)
 
 		failed = false;	/* haven't failed authentication yet */
 		if (NULL == username) {	/* need to get a login id */
-			size_t  max_size = sysconf(_SC_LOGIN_NAME_MAX);
+			size_t  max_size;
 
+			max_size = login_name_max_size();
 			if (subroot) {
 				closelog ();
 				exit (1);