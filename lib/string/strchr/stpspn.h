53780f68b44dddc0b687ae0eda21d4d34ba34fd3
Alejandro Colomar <alx@kernel.org>
2024-05-18 20:41:30 +0200

    lib/string/strchr/: stp[c]spn(), strrspn(), strnul(): Add macros and functions
    Often, a pointer is more useful than a length when calling these.
    
    Link: <https://docs.oracle.com/cd/E86824_01/html/E54769/strrspn-3gen.html>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strchr/stpspn.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCHR_STPSPN_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCHR_STPSPN_H_
+
+
+#include <config.h>
+
+#include <string.h>
+
+#include "attr.h"
+
+
+// Similar to strspn(3), but return a pointer instead of an offset.
+// Similar to strchrnul(3), but search for any bytes not in 'accept'.
+#define stpspn(s, accept)                                                     \
+({                                                                            \
+	__auto_type  s_ = s;                                                  \
+                                                                              \
+	s_ + strspn(s_, accept);                                              \
+})
+
+
+#endif  // include guard
---
+++ lib/string/strchr/strrspn.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "string/strchr/strrspn.h"
+
+
+extern inline char *strrspn(char *restrict s, const char *restrict accept);
