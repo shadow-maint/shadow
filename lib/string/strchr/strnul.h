2fcf5201849d77e4782dc1b41393d89711cd8d90
Alejandro Colomar <alx@kernel.org>
2024-05-18 20:41:30 +0200

    lib/string/strchr/: stp[c]spn(), strrspn(), strnul(): Add macros and functions
    Often, a pointer is more useful than a length when calling these.
    
    Link: <https://docs.oracle.com/cd/E86824_01/html/E54769/strrspn-3gen.html>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strchr/strnul.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCHR_STRNUL_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCHR_STRNUL_H_
+
+
+#include <config.h>
+
+#include <string.h>
+
+#include "attr.h"
+
+
+// Similar to strlen(3), but return a pointer instead of an offset.
+#define strnul(s)                                                             \
+({                                                                            \
+	__auto_type  s_ = s;                                                  \
+                                                                              \
+	s_ + strlen(s_);                                                      \
+})
+
+
+#endif  // include guard
---
+++ lib/string/strchr/strrspn.h
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCHR_STRRSPN_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCHR_STRRSPN_H_
+
+
+#include <config.h>
+
+#include <string.h>
+
+#include "attr.h"
+#include "string/strchr/strnul.h"
+
+
+ATTR_STRING(2)
+inline char *strrspn(char *restrict s, const char *restrict accept);
+
+
+// Available in Oracle Solaris: strrspn(3GEN).
+// <https://docs.oracle.com/cd/E36784_01/html/E36877/strrspn-3gen.html>
+inline char *
+strrspn(char *restrict s, const char *restrict accept)
+{
+	char  *p;
+
+	p = strnul(s);
+	while (p > s) {
+		p--;
+		if (strchr(accept, *p) == NULL)
+			return p + 1;
+	}
+	return s;
+}
+
+
+#endif  // include guard
