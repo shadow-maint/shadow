f3b843caba8c252cb43bdd08b8b7926c8440afe8
Alejandro Colomar <alx@kernel.org>
2024-05-18 20:41:30 +0200

    lib/string/strchr/: stp[rc]spn(), strnul(): Add macros and functions
    Ofter, a pointer is more useful than a length when calling these.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strchr/stpspn.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCHR_STPSPN_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCHR_STPSPN_H_
+
+
+#include <config.h>
+
+#include <string.h>
+
+#include "attr.h"
+
+
+// Similar to strspn(3), but return a pointer.
+#define stpspn(s, accept)                                                     \
+({                                                                            \
+	__auto_type  s_ = s;                                                  \
+                                                                              \
+	s_ + strspn(s_, accept);                                              \
+})
+
+
+#endif  // include guard
