1d2acea1e5be9072452a3a944eebe981c0c7256f
Alejandro Colomar <alx@kernel.org>
2024-05-21 12:50:27 +0200

    lib/string/strcpy/strncat.[ch]: STRNCAT(): Add macro
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strcpy/strncat.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCPY_STRNCAT_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCPY_STRNCAT_H_
+
+
+#include <config.h>
+
+#include <string.h>
+
+#include "sizeof.h"
+
+
+#define STRNCAT(dst, src)  strncat(dst, src, NITEMS(src))
+
+
+#endif  // include guard
