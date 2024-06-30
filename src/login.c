shadow-maint/shadow

commit 3704745289f58d10cbeb1bde26849989487e66f1

* lib/defines.h: Define USER_NAME_MAX_LENGTH, based on utmp and 	default to 32. 	* libmisc/chkname.c: Use USER_NAME_MAX_LENGTH. 	* src/login.c: Use USER_NAME_MAX_LENGTH instead of the default 32. 	username also needs to be bigger than USER_NAME_MAX_LENGTH because 	it has to be nul-terminated.

src/login.c
@@ -610,8 +610,9 @@ int main (int argc, char **argv)
 	}
 #ifdef RLOGIN
 	if (rflg) {
-		username = xmalloc (32 * sizeof (char));
-		if (do_rlogin (hostname, username, 32, term, sizeof term)) {
+		username = xmalloc (USER_NAME_MAX_LENGTH + 1);
+		username[USER_NAME_MAX_LENGTH] = '\0';
+		if (do_rlogin (hostname, username, USER_NAME_MAX_LENGTH, term, sizeof term)) {
 			preauth_flag = true;
 		} else {
 			free (username);
@@ -920,8 +921,9 @@ int main (int argc, char **argv)
 				exit (1);
 			}
 			preauth_flag = false;
-			username = xmalloc (32);
-			login_prompt (_("\n%s login: "), username, 32);
+			username = xmalloc (USER_NAME_MAX_LENGTH + 1);
+			username[USER_NAME_MAX_LENGTH] = '\0';
+			login_prompt (_("\n%s login: "), username, USER_NAME_MAX_LENGTH);
 
 			if ('\0' == username) {
 				/* Prompt for a new login */

lib/defines.h
@@ -356,4 +356,19 @@ extern char *strerror ();
 #define MAX(x,y) (((x) > (y)) ? (x) : (y))
 #endif
 
+/* Maximum length of usernames */
+#ifdef HAVE_UTMPX_H
+# define USER_NAME_MAX_LENGTH (sizeof (((struct utmpx *)NULL)->ut_user))
+#else
+# ifdef HAVE_STRUCT_UTMP_UT_USER
+#  define USER_NAME_MAX_LENGTH (sizeof (((struct utmp *)NULL)->ut_user))
+# else
+#  ifdef HAVE_STRUCT_UTMP_UT_NAME
+#   define USER_NAME_MAX_LENGTH (sizeof (((struct utmp *)NULL)->ut_name))
+#  else
+#   define USER_NAME_MAX_LENGTH 32
+#  endif
+# endif
+#endif
+
 #endif				/* _DEFINES_H_ */

libmisc/chkname.c
@@ -77,17 +77,11 @@ static bool is_valid_name (const char *name)
 
 bool is_valid_user_name (const char *name)
 {
-#if HAVE_UTMPX_H
-	struct utmpx ut;
-#else
-	struct utmp ut;
-#endif
-
 	/*
 	 * User names are limited by whatever utmp can
 	 * handle.
 	 */
-	if (strlen (name) > sizeof (ut.ut_user)) {
+	if (strlen (name) > USER_NAME_MAX_LENGTH) {
 		return false;
 	}
 

commit 29dfe54c1e913cb600c34e40ab601aaef93d7150

src/login.c: Remove dead code
The functions that set these strings --do_rlogin() and login_prompt()--
make sure to terminate them with a NUL.

Fixes: 3704745289f5 ("* lib/defines.h: Define USER_NAME_MAX_LENGTH, based on utmp and [...]")
Signed-off-by: Alejandro Colomar <alx@kernel.org>

src/login.c
@@ -834,7 +834,6 @@ int main (int argc, char **argv)
 			}
 			preauth_flag = false;
 			username = XMALLOC(max_size, char);
-			username[max_size - 1] = '\0';
 			login_prompt(username, max_size);
 
 			if ('\0' == username[0]) {