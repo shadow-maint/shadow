53579f0e1e3b572510f2bd352e9829ca19ecaece
Alejandro Colomar <alx@kernel.org>
2024-07-11 12:45:13 +0200

    Depend on liba2i
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/atoi/str2i.c
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2007-2009, Nicolas François
-// SPDX-FileCopyrightText: 2023-2025, Alejandro Colomar <alx@kernel.org>
-// SPDX-License-Identifier: BSD-3-Clause
-
-
-#include <config.h>
-
-#include "atoi/str2i.h"
