538796940c8d4576da14c74b0768591a4542dc4a
Alejandro Colomar <alx@kernel.org>
2024-07-21 18:40:25 +0200

    lib/, po/: Remove fgetsx() and fputsx()
    It seems they never worked correctly.  Let's keep it simple and remove
    support for escaped newlines.
    
    Closes: <https://github.com/shadow-maint/shadow/issues/1055>
    Reported-by: Chris Hofstaedtler <zeha@debian.org>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/gshadow.c
+++ lib/gshadow.c
@@ -144,7 +144,7 @@ sgetsgent(const char *string)
 		return NULL;
 	}
 
-	if (fgetsx(buf, buflen, fp) == NULL)
+	if (fgets(buf, buflen, fp) == NULL)
 		return NULL;
 
 	while (   (strrchr(buf, '\n') == NULL)
@@ -159,7 +159,7 @@ sgetsgent(const char *string)
 		buflen *= 2;
 
 		len = strlen (buf);
-		if (fgetsx(&buf[len], buflen - len, fp) == NULL)
+		if (fgets(&buf[len], buflen - len, fp) == NULL)
 			return NULL;
 	}
 	stpsep(buf, "\n");
@@ -258,11 +258,7 @@ int putsgent (const struct sgrp *sgrp, FILE * fp)
 	}
 	stpcpy(cp, "\n");
 
-	/*
-	 * Output using the function which understands the line
-	 * continuation conventions.
-	 */
-	if (fputsx (buf, fp) == EOF) {
+	if (fputs(buf, fp) == EOF) {
 		free (buf);
 		return -1;
 	}
