10cf7b3c7ecf28cee9991eaffb76c5575e901fcc
Alejandro Colomar <alx@kernel.org>
2024-07-26 11:13:37 +0200

    lib/string/strcmp/: strprefix(): Add function
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strcmp/strprefix.h
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCMP_STRPREFIX_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCMP_STRPREFIX_H_
+
+
+#include <config.h>
+
+#include <stddef.h>
+#include <string.h>
+
+#include "attr.h"
+
+
+ATTR_STRING(1)
+ATTR_STRING(2)
+inline char *strprefix(const char *s, const char *prefix);
+
+
+/*
+ * Return NULL if s does not start with prefix.
+ * Return `s + strlen(prefix)` if s starts with prefix.
+ */
+inline char *
+strprefix(const char *s, const char *prefix)
+{
+	if (strncmp(s, prefix, strlen(prefix)) != 0)
+		return NULL;
+
+	return (char *) s + strlen(prefix);
+}
+
+
+#endif  // include guard
