029ebe5ad6cbbcb62d33cd3b3d03d9bc0c0ca60c
Tobias Stoeckmann <tobias@stoeckmann.org>
2024-09-13 23:38:15 +0200

    lib: Fix typo
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- lib/shadowlog_internal.h
+++ lib/shadowlog_internal.h
@@ -1,7 +1,7 @@
 #ifndef _SHADOWLOG_INTERNAL_H
 #define _SHADOWLOG_INTERNAL_H
 
-extern const char *shadow_progname; /* Program name showed in error messages */
+extern const char *shadow_progname; /* Program name shown in error messages */
 extern FILE *shadow_logfd;  /* file descriptor to which error messages are printed */
 
 #endif /* _SHADOWLOG_INTERNAL_H */
