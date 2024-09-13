9090b6fd3238b5c63099cefa364d359fba52d93d
Tobias Stoeckmann <tobias@stoeckmann.org>
2024-09-13 21:50:00 +0200

    lib/run_part: Reduce visibility
    The run_part function is only used in run_part.c itself, so no
    need to expose it to other files.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- lib/run_part.h
+++ lib/run_part.h
@@ -1,7 +1,6 @@
 #ifndef _RUN_PART_H
 #define _RUN_PART_H
 
-int run_part (char *script_path, const char *name, const char *action);
 int run_parts (const char *directory, const char *name, const char *action);
 
 #endif /* _RUN_PART_H */
