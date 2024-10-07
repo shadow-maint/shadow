26976888fc9807f56b9135cc004efd880be3e25e
Iker Pedrosa <ipedrosa@redhat.com>
2024-10-07 15:44:17 +0200

    Tests: implement system test framework
    As discussed at length, this is the implementation of the new system
    tests framework for shadow. This is a proof of concept that contains the
    key elements to be able to run basic user (i.e. useradd, usermod) and
    group (i.e. usermod) tests. If you like the framework the rest of the
    functionality will be added in the future.
    
    Some useful facts:
    * It is implemented in python
    * It is based on pytest and pytest-mh
    * It works on all the distributions that are part of our CI
    * It can be run in the cloud (VM or container) as well as on-premises
    * After the execution of each test the environment is cleaned up
    * Logs and other artifacts for failed tests are collected
    * It has a rich API that can be extended and extended to cover new
      functionalities
    
    Closes: https://github.com/shadow-maint/shadow/issues/835
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

---
+++ tests/system/framework/roles/shadow.py
@@ -0,0 +1,129 @@
+"""shadow multihost role."""
+
+from __future__ import annotations
+
+import shlex
+from typing import Dict
+
+from pytest_mh.conn import ProcessLogLevel, ProcessResult
+
+from ..hosts.shadow import ShadowHost
+from .base import BaseLinuxRole
+
+__all__ = [
+    "Shadow",
+]
+
+
+class Shadow(BaseLinuxRole[ShadowHost]):
+    """
+    shadow role.
+
+    Provides unified Python API for managing and testing shadow.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """
+        Set up the environment.
+        """
+        super().__init__(*args, **kwargs)
+
+    def teardown(self) -> None:
+        """
+        Detect file mismatches before cleaning up the environment.
+        """
+        self.host.detect_file_mismatches()
+        """
+        Clean up the environment.
+        """
+        super().teardown()
+
+    def _parse_args(self, *args) -> Dict[str, str]:
+        args_list = shlex.split(*args[0])
+        name = args_list[-1]
+
+        return {"name": name}
+
+    def useradd(self, *args) -> ProcessResult:
+        """
+        Create user.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Creating user "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("useradd " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/passwd")
+        self.host.discard_file("/etc/shadow")
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
+
+    def usermod(self, *args) -> ProcessResult:
+        """
+        Modify user.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Modifying user "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("usermod " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/passwd")
+        self.host.discard_file("/etc/shadow")
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
+
+    def userdel(self, *args) -> ProcessResult:
+        """
+        Delete user.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Deleting user "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("userdel " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/passwd")
+        self.host.discard_file("/etc/shadow")
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
+
+    def groupadd(self, *args) -> ProcessResult:
+        """
+        Create group.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Creating group "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("groupadd " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
+
+    def groupmod(self, *args) -> ProcessResult:
+        """
+        Modify group.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Modifying group "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("groupmod " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
+
+    def groupdel(self, *args) -> ProcessResult:
+        """
+        Delete group.
+        """
+        args_dict = self._parse_args(args)
+        self.logger.info(f'Deleting group "{args_dict["name"]}" on {self.host.hostname}')
+        cmd = self.host.conn.run("groupdel " + args[0], log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
