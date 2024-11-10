712e94a2d076b2aad75bb077e1204d5d76dd9408
Alejandro Colomar <alx@kernel.org>
2024-11-10 16:22:12 +0100

    lib/shadow/, lib/: gshadow: Move to separate file and rename
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/shadow/gshadow/gshadow.c
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 1990-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-1998, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2005, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2008-2009, Nicolas François
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include "config.h"
+
+#include "shadow/gshadow/gshadow.h"
+
+#include <stddef.h>
+#include <stdio.h>
+
+
+FILE  *gshadow = NULL;
