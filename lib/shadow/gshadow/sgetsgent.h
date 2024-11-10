a7822d17860d9eac3730a62909a2d8b9e505aedb
Alejandro Colomar <alx@kernel.org>
2024-11-10 18:44:05 +0100

    lib/shadow/gshadow/, lib/: sgetsgent(): Move to separate file
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/shadow/gshadow/sgetsgent.h
+++ lib/shadow/gshadow/sgetsgent.h
@@ -5,13 +5,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 
-#ifndef SHADOW_INCLUDE_LIB_GSHADOW__H_
-#define SHADOW_INCLUDE_LIB_GSHADOW__H_
-
-
-#if defined(HAVE_GSHADOW_H)
-# include <gshadow.h>
-#else
+#ifndef SHADOW_INCLUDE_LIB_SHADOW_GSHADOW_SGETSGENT_H_
+#define SHADOW_INCLUDE_LIB_SHADOW_GSHADOW_SGETSGENT_H_
 
 
 #include <config.h>
@@ -19,11 +14,11 @@
 #include "shadow/gshadow/sgrp.h"
 
 
-/*@observer@*//*@null@*/struct sgrp *getsgent (void);
-/*@observer@*//*@null@*/struct sgrp *getsgnam (const char *);
+#if defined(HAVE_GSHADOW_H)
+# include <gshadow.h>
+#else
+struct sgrp *sgetsgent(const char *);
+#endif
 
-#define	GSHADOW	"/etc/gshadow"
 
-
-#endif  // !HAVE_GSHADOW_H
 #endif  // include guard
