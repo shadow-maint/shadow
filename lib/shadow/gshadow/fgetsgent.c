147ea31c051134c6faebd38cc5267e3d15b0e056
Alejandro Colomar <alx@kernel.org>
2024-11-10 18:18:19 +0100

    lib/shadow/gshadow/, lib/: fgetsgent(): Move to separate file
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/shadow/gshadow/fgetsgent.c
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: 1990-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-1998, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2005, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2008-2009, Nicolas François
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include "config.h"
+
+#include "shadow/gshadow/fgetsgent.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "alloc/malloc.h"
+#include "alloc/realloc.h"
+#include "defines.h"
+#include "prototypes.h"
+#include "shadow/gshadow/sgrp.h"
+#include "string/strtok/stpsep.h"
+
+
+/*
+ * fgetsgent - convert next line in stream to (struct sgrp)
+ *
+ * fgetsgent() reads the next line from the provided stream and
+ * converts it to a (struct sgrp).  NULL is returned on EOF.
+ */
+#if defined(SHADOWGRP) && !__has_include(<gshadow.h>)
+// from-FILE get-next shadow group entry
+struct sgrp *
+fgetsgent(FILE *fp)
+{
+	static size_t buflen = 0;
+	static char *buf = NULL;
+
+	char *cp;
+
+	if (0 == buflen) {
+		buf = MALLOC(BUFSIZ, char);
+		if (NULL == buf) {
+			return NULL;
+		}
+		buflen = BUFSIZ;
+	}
+
+	if (NULL == fp) {
+		return NULL;
+	}
+
+	if (fgetsx(buf, buflen, fp) == NULL)
+		return NULL;
+
+	while (   (strrchr(buf, '\n') == NULL)
+	       && (feof (fp) == 0)) {
+		size_t len;
+
+		cp = REALLOC(buf, buflen * 2, char);
+		if (NULL == cp) {
+			return NULL;
+		}
+		buf = cp;
+		buflen *= 2;
+
+		len = strlen (buf);
+		if (fgetsx (&buf[len],
+			    (int) (buflen - len),
+			    fp) != &buf[len]) {
+			return NULL;
+		}
+	}
+	stpsep(buf, "\n");
+	return (sgetsgent (buf));
+}
+#endif
---
+++ lib/shadow/gshadow/fgetsgent.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 1988-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-1997, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2003-2005, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_SHADOW_GSHADOW_FGETSGENT_H_
+#define SHADOW_INCLUDE_LIB_SHADOW_GSHADOW_FGETSGENT_H_
+
+
+#include "config.h"
+
+#include <stdio.h>
+
+#include "shadow/gshadow/sgrp.h"
+
+
+#if __has_include(<gshadow.h>)
+# include <gshadow.h>
+#else
+struct sgrp *fgetsgent(FILE *stream);
+#endif
+
+
+#endif  // include guard
