6589aad5409de825692fdc878c46aac075c71fb4
Alejandro Colomar <alx@kernel.org>
2024-11-14 19:54:33 +0100

    lib/string/strchr/: strchrscnt(): Add function
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strchr/strchrscnt.c
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "string/strchr/strchrscnt.h"
+
+#include <stddef.h>
+
+
+extern inline size_t strchrscnt(const char *s, const char *c);
