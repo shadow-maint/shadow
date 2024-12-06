a2dd29c825996e5f0f0d6538306dea13b50c3243
Alejandro Colomar <alx@kernel.org>
2024-12-06 21:21:24 +0100

    lib/: Use STRSEP2ARR() instead of its pattern
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/gshadow.c
+++ lib/gshadow.c
@@ -26,12 +26,11 @@
 #include "string/strchr/strchrcnt.h"
 #include "string/strcmp/streq.h"
 #include "string/strtok/stpsep.h"
+#include "string/strtok/strsep2arr.h"
 
 
 static /*@null@*/FILE *shadow;
 static struct sgrp  sgroup = {};
-
-#define	FIELDS	4
 
 
 static /*@null@*/char **
@@ -74,9 +73,7 @@ sgetsgent(const char *string)
 	static char *sgrbuf = NULL;
 	static size_t sgrbuflen = 0;
 
-	char *fields[FIELDS];
-	char *cp;
-	int i;
+	char  *fields[4];
 	size_t len = strlen (string) + 1;
 
 	if (len > sgrbuflen) {
@@ -91,20 +88,7 @@ sgetsgent(const char *string)
 	strcpy (sgrbuf, string);
 	stpsep(sgrbuf, "\n");
 
-	/*
-	 * There should be exactly 4 colon separated fields.  Find
-	 * all 4 of them and save the starting addresses in fields[].
-	 */
-
-	for (cp = sgrbuf, i = 0; (i < FIELDS) && (NULL != cp); i++)
-		fields[i] = strsep(&cp, ":");
-
-	/*
-	 * If there was an extra field somehow, or perhaps not enough,
-	 * the line is invalid.
-	 */
-
-	if (NULL != cp || i != FIELDS)
+	if (STRSEP2ARR(sgrbuf, ":", fields) == -1)
 		return NULL;
 
 	sgroup.sg_name = fields[0];
