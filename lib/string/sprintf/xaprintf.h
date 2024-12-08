e645cc0d2822a5ddbd3bbe7b0a9b2e5d0f65493b
Alejandro Colomar <alx@kernel.org>
2024-12-08 12:21:06 +0100

    lib/string/sprintf/, tests/: xaprintf(): Reimplement in terms of exit_if_null()
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/string/sprintf/xaprintf.h
+++ lib/string/sprintf/xaprintf.h
@@ -8,52 +8,12 @@
 
 #include "config.h"
 
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "attr.h"
 #include "string/sprintf/aprintf.h"
-
-
-ATTR_MALLOC(free)
-format_attr(printf, 1, 2)
-inline char *xaprintf(const char *restrict fmt, ...);
-
-ATTR_MALLOC(free)
-format_attr(printf, 1, 0)
-inline char *xvaprintf(const char *restrict fmt, va_list ap);
+#include "exit_if_null.h"
 
 
 // exit-on-error allocate print formatted
-inline char *
-xaprintf(const char *restrict fmt, ...)
-{
-	char     *p;
-	va_list  ap;
-
-	va_start(ap, fmt);
-	p = xvaprintf(fmt, ap);
-	va_end(ap);
-
-	return p;
-}
-
-
-inline char *
-xvaprintf(const char *restrict fmt, va_list ap)
-{
-	char  *p;
-
-	p = vaprintf(fmt, ap);
-	if (p == NULL) {
-		perror("vaprintf");
-		exit(EXIT_FAILURE);
-	}
-
-	return p;
-}
+#define xaprintf(...)  exit_if_null(aprintf(__VA_ARGS__))
 
 
 #endif  // include guard
