5a7140c2855d66781263e19fb643535d71fe41c5
Alejandro Colomar <alx@kernel.org>
2024-12-09 13:04:50 +0100

    src/: Report errors in user or groups names more consistently
    -  Don't print the user name; if it's bad, it might be dangerous.
    -  Print the string "user" or "group" before the error message.
    -  Print the errno string to be consistent.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/usermod.c
+++ src/usermod.c
@@ -1131,9 +1131,8 @@ process_flags(int argc, char **argv, struct option_flags *flags)
 				/*@notreached@*/break;
 			case 'l':
 				if (!is_valid_user_name(optarg)) {
-					fprintf(stderr,
-						_("%s: invalid user name '%s'\n"),
-						Prog, optarg);
+					fprintf(stderr, _("%s: user: %s\n"),
+						Prog, strerrno());
 					exit (E_BAD_ARG);
 				}
 				lflg = true;
