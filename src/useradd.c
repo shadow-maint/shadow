331019c7e6e76137809ce4faa15bead01e0c59d1
Alejandro Colomar <alx@kernel.org>
2024-12-09 12:50:51 +0100

    Little Bobby Tables
    lib/, man/, src/: Do not allow bad names
    
    Closes: <https://github.com/shadow-maint/shadow/issues/1149>
    Link: <https://www.wired.com/story/null-license-plate-landed-one-hacker-ticket-hell/>
    Link: <https://xkcd.com/327/>
    Link: <https://www.youtube.com/watch?v=hNoS2BU6bbQ>
    Link: <https://lwn.net/Articles/1001215/>
    Link: <https://dwheeler.com/essays/fixing-unix-linux-filenames.html>
    Link: <https://github.com/shadow-maint/shadow/issues/121>
    Link: <https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=42874>
    Link: <https://lists.debian.org/debian-devel/2024/11/msg00250.html>
    Link: <https://lists.debian.org/debian-devel/2024/12/msg00012.html>
    Link: <https://lwn.net/Articles/1000485/>
    Cc: Iker Pedrosa <ipedrosa@redhat.com>
    Cc: Serge Hallyn <serge@hallyn.com>
    Cc: Sam James <sam@gentoo.org>
    Cc: Michael Vetter <jubalh@iodoru.org>
    Cc: Chris Hofstaedtler <zeha@debian.org>
    Cc: Balint Reczey <rbalint@debian.org>
    Cc: Marc Haber <mh+githubvisible@zugschlus.de>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/useradd.c
+++ src/useradd.c
@@ -160,7 +160,6 @@ static char **user_groups;	/* NULL-terminated list */
 static long sys_ngroups;
 static bool do_grp_update = false;	/* group files need to be updated */
 
-extern int allow_bad_names;
 
 static bool
     bflg = false,		/* new default root of home directory */
@@ -877,7 +876,6 @@ static void usage (int status)
 	                  "\n"
 	                  "Options:\n"),
 	                Prog, Prog, Prog);
-	(void) fputs (_("      --badname                 do not check for bad names\n"), usageout);
 	(void) fputs (_("  -b, --base-dir BASE_DIR       base directory for the home directory of the\n"
 	                "                                new account\n"), usageout);
 #ifdef WITH_BTRFS
@@ -1141,7 +1139,6 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 #ifdef WITH_BTRFS
 			{"btrfs-subvolume-home", no_argument, NULL, 200},
 #endif
-			{"badname",        no_argument,       NULL, 201},
 			{"comment",        required_argument, NULL, 'c'},
 			{"home-dir",       required_argument, NULL, 'd'},
 			{"defaults",       no_argument,       NULL, 'D'},
@@ -1197,9 +1194,6 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 				break;
 			case 200:
 				subvolflg = true;
-				break;
-			case 201:
-				allow_bad_names = true;
 				break;
 			case 'c':
 				if (!VALID (optarg)) {
@@ -1498,15 +1492,9 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 
 		user_name = argv[optind];
 		if (!is_valid_user_name(user_name)) {
-			if (errno == EILSEQ) {
-				fprintf(stderr,
-				        _("%s: invalid user name '%s': use --badname to ignore\n"),
-				        Prog, user_name);
-			} else {
-				fprintf(stderr,
-				        _("%s: invalid user name '%s'\n"),
-				        Prog, user_name);
-			}
+			fprintf(stderr,
+			        _("%s: invalid user name '%s'\n"),
+			        Prog, user_name);
 #ifdef WITH_AUDIT
 			audit_logger (AUDIT_ADD_USER,
 			              "add-user",
