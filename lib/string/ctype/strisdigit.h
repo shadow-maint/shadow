40c9035bfd75c53f16cdef56906ec3ed3d4dac5f
Alejandro Colomar <alx@kernel.org>
2024-12-11 01:57:29 +0100

    lib/string/ctype/: strisdigit(): Add function
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/ctype/strisdigit.h
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_CTYPE_STRISDIGIT_H_
+#define SHADOW_INCLUDE_LIB_STRING_CTYPE_STRISDIGIT_H_
+
+
+#include <config.h>
+
+#include <stdbool.h>
+
+#include "string/strchr/stpspn.h"
+#include "string/strcmp/streq.h"
+
+
+inline bool strisdigit(const char *s);
+
+
+// Like isdigit(3), but check all characters in the string
+inline bool
+strisdigit(const char *s)
+{
+	if (streq(s, ""))
+		return false;
+
+	return streq(stpspn(s, "0123456789"), "");
+}
+
+
+#endif  // include guard
