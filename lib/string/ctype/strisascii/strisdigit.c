ceef9e1a21a8fbbee463c860248b943037ac0d66
Alejandro Colomar <alx@kernel.org>
2024-12-11 01:57:29 +0100

    lib/string/ctype/strisascii/: strisdigit(): Add function
    Reviewed-by: Serge Hallyn <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/ctype/strisascii/strisdigit.c
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "string/ctype/strisascii/strisdigit.h"
+
+#include <stdbool.h>
+
+
+extern inline bool strisdigit(const char *s);
