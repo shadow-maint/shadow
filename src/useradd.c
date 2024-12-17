8562d134028b2df9f3721de99cee221967238536
sgakerru <sulmpx60@yandex.ru>
2024-12-17 19:18:38 +0400

    src/useradd.c: Use free_list() to free user groups list

--- src/useradd.c
+++ src/useradd.c
@@ -751,11 +751,7 @@ static int get_groups (char *list, struct option_flags *flags)
 	/*
 	 * Free previous group list before creating a new one.
 	 */
-	int i = 0;
-	while (NULL != user_groups[i]) {
-		free(user_groups[i]);
-		user_groups[i++] = NULL;
-	}
+	free_list(user_groups);
 
 	if (streq(list, "")) {
 		return 0;
