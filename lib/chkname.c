81c2ba628fdc59d07039e169b718e5ab43fce153
Alejandro Colomar <alx@kernel.org>
2024-12-23 15:25:48 +0100

    lib/chkname.c: is_valid_name(): Use isalnum(3) instead of its pattern
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/chkname.c
+++ lib/chkname.c
@@ -69,9 +69,7 @@ is_valid_name(const char *name)
 	 * sake of Samba 3.x "add machine script"
 	 */
 
-	if (!((*name >= 'a' && *name <= 'z') ||
-	      (*name >= 'A' && *name <= 'Z') ||
-	      (*name >= '0' && *name <= '9') ||
+	if (!(isalnum(*name) ||
 	      *name == '_' ||
 	      *name == '.'))
 	{
@@ -80,9 +78,7 @@ is_valid_name(const char *name)
 	}
 
 	while (!streq(++name, "")) {
-		if (!((*name >= 'a' && *name <= 'z') ||
-		      (*name >= 'A' && *name <= 'Z') ||
-		      (*name >= '0' && *name <= '9') ||
+		if (!(isalnum(*name) ||
 		      *name == '_' ||
 		      *name == '.' ||
 		      *name == '-' ||
