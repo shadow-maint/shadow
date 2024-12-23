5ddbdca8ba3ddd1c19f086c2a130793e8d5fd1d8
Alejandro Colomar <alx@kernel.org>
2024-12-23 11:06:33 +0100

    lib/chkname.c, src/: Strictly disallow really bad names
    Some names are bad, and some names are really bad.  '--badname' should
    only allow the mildly bad ones, which we can handle.  Some names are too
    bad, and it's not possible to deal with them.  Reject them
    unconditionally.
    
    Acked-by: Chris Hofstaedtler <zeha@debian.org>
    Cc: Marc 'Zugschlus' Haber <mh+githubvisible@zugschlus.de>
    Cc: Iker Pedrosa <ipedrosa@redhat.com>
    Cc: Serge Hallyn <serge@hallyn.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/pwck.c
+++ src/pwck.c
@@ -475,7 +475,7 @@ static void check_pw_file (int *errors, bool *changed)
 		 */
 
 		if (!is_valid_user_name(pwd->pw_name)) {
-			if (errno == EINVAL) {
+			if (errno == EILSEQ) {
 				printf(_("invalid user name '%s': use --badname to ignore\n"),
 				       pwd->pw_name);
 			} else {
