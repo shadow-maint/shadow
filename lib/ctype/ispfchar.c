5d39b610b1653ade45c931a6ae8bf04a62292c06
Alejandro Colomar <alx@kernel.org>
2024-12-23 18:36:31 +0100

    lib/ctype/: ispfchar(): Add function
    This function returns true if the input character is a character from
    the POSIX portable filename character set.
    
    Link: <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/V1_chap03.html#tag_03_265>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/ctype/ispfchar.c
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "ctype/ispfchar.h"
+
+#include <stdbool.h>
+
+
+extern inline bool ispfchar(unsigned char c);
