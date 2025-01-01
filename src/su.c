1e7bb9cb17d8844b4699948511e9b92ac2df1850
Alejandro Colomar <alx@kernel.org>
2025-01-01 14:41:18 +0100

    lib/, src/: Use xaprintf() instead of xasprintf()
    This makes some temporary variables unnecessary.  They'll be removed in
    the next commit.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/su.c
+++ src/su.c
@@ -60,7 +60,7 @@
 #include "prototypes.h"
 #include "shadowlog.h"
 #include "string/sprintf/snprintf.h"
-#include "string/sprintf/xasprintf.h"
+#include "string/sprintf/xaprintf.h"
 #include "string/strcmp/streq.h"
 #include "string/strcpy/strtcpy.h"
 #include "string/strdup/xstrdup.h"
@@ -1211,7 +1211,7 @@ int main (int argc, char **argv)
 			cp = Basename (shellstr);
 		}
 
-		xasprintf(&arg0, "-%s", cp);
+		arg0 = xaprintf("-%s", cp);
 		cp = arg0;
 	} else {
 		cp = Basename (shellstr);
