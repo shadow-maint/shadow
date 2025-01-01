2ac1426e975db2a12dd2f98f7529fe3b01473ddf
Alejandro Colomar <alx@kernel.org>
2025-01-02 00:12:51 +0100

    lib/, src/, tests/: Move x*() definitions to non-x* header files
    Now that all of these are one-liners, they don't need a separate header
    file.  Compact stuff.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/alloc/calloc.h
+++ lib/alloc/calloc.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023-2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2023-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -10,11 +10,16 @@
 
 #include <stdlib.h>
 
+#include "x.h"
+
 
 #define CALLOC(n, type)                                                       \
 (                                                                             \
 	(type *) calloc(n, sizeof(type))                                      \
 )
+
+
+#define XCALLOC(n, type)  X(CALLOC(n, type))
 
 
 #endif  // include guard
