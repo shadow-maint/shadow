766982e16eb037c2090985935a37eb97e37fe405
Alejandro Colomar <alx@kernel.org>
2025-02-05 18:42:21 +0100

    lib/string/strcmp/: strcaseeq(): Add function
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strcmp/strcaseeq.c
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include <stdbool.h>
+
+#include "string/strcmp/strcaseeq.h"
+
+
+extern inline bool strcaseeq(const char *s1, const char *s2);
---
+++ lib/string/strcmp/strcaseeq.h
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_STRCMP_STRCASEEQ_H_
+#define SHADOW_INCLUDE_LIB_STRING_STRCMP_STRCASEEQ_H_
+
+
+#include <config.h>
+
+#include <stdbool.h>
+#include <strings.h>
+
+#include "attr.h"
+
+
+ATTR_STRING(1) ATTR_STRING(2)
+inline bool strcaseeq(const char *s1, const char *s2);
+
+
+// streq(), but case-insensitive.
+inline bool
+strcaseeq(const char *s1, const char *s2)
+{
+	return strcasecmp(s1, s2) == 0;
+}
+
+
+#endif  // include guard
