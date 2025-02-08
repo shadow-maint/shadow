93493077a1497f4e1a3f76b1eac3fc119a367437
Alejandro Colomar <alx@kernel.org>
2025-02-08 10:57:51 +0100

    lib/string/strcmp/: strcaseprefix(): Add API
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strcmp/strcaseprefix.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include "string/strcmp/strcaseprefix.h"
+
+
+extern inline const char *strcaseprefix_(const char *s, const char *prefix);
