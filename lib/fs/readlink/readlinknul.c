804a65874117953cb635abb7f2ab8ec40f3b1fc1
Alejandro Colomar <alx@kernel.org>
2025-02-09 12:23:46 +0100

    lib/fs/readlink/: readlinknul(): Use ssize_t to simplify
    Consistently using a signed type allows us to avoid sign-mismatch
    diagnostics, while keeping the code simple.  It feels weird to
    accept a ssize_t instead of a size_t, but it's a matter of getting
    used to it.
    
    Another way to achieve this with a single 'len' variable and no casts
    would be to compare against SIZE_MAX, but that's less readable than -1.
    Or one could write a SIZE_C() macro a la UINT64_C(), and compare the
    size_t against SIZE_C(-1), but that's still suboptimal (regarding
    readability) compared to consistently using signed size types.
    
    Fixes: b9d00b64a19f (2024-12-09; "lib/fs/readlink/readlinknul.h: readlinknul(): Silence warning")
    Acked-by: Serge Hallyn <serge@hallyn.com>
    Cc: Martin Uecker <uecker@tugraz.at>
    Cc: "Robert C. Seacord" <rcseacord@gmail.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/fs/readlink/readlinknul.c
+++ lib/fs/readlink/readlinknul.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -6,9 +6,8 @@
 
 #include "fs/readlink/readlinknul.h"
 
-#include <stddef.h>
 #include <sys/types.h>
 
 
 extern inline ssize_t readlinknul(const char *restrict link, char *restrict buf,
-    size_t size);
+    ssize_t size);
