8da922e7747f32b81176050ab12f2a0e9fffca07
Alejandro Colomar <alx@kernel.org>
2025-02-10 15:06:34 +0100

    lib/, po/, src/: Rename stpeprintf() => seprintf()
    The old name was too complex, and is inconsistent with all other
    sprintf(3)-based APIs having just one letter for differentiation.
    This allows breaking less lines.
    
    The original name was chosen for differentiation with the buggy Plan9
    API seprint(2).  However, 9front (the current fork where Plan9 is mainly
    developed) has acknowledged the bug.  There's still no decision on
    fixing the bug or not, due to the age of their code base, and the
    projects depending on their library.  It is under consideration
    inventing something like a seprint2(2) in 9front for replacement of
    seprint(2), but there's no decision yet either.
    
    Considering that 9front acknowledges their bug, and that they *may*
    release a fixed API with a similar name, we may as well claim that our
    seprintf() is also a fixed version of Plan9's seprint(2).  It has a
    different name, after all (we terminate in 'f').
    
    This commit was partially scripted with
    
    	$ find * -type f \
    	| xargs grep -l stpeprintf \
    	| xargs sed -i 's/stpeprintf/seprintf/g';
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/sprintf/seprintf.h
@@ -0,0 +1,65 @@
+// SPDX-FileCopyrightText: 2022-2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_STRING_SPRINTF_SEPRINTF_H_
+#define SHADOW_INCLUDE_LIB_STRING_SPRINTF_SEPRINTF_H_
+
+
+#include "config.h"
+
+#include <stdarg.h>
+#include <stddef.h>
+
+#include "attr.h"
+#include "string/sprintf/stprintf.h"
+
+
+#if !defined(HAVE_SEPRINTF)
+// seprintf - string end-pointer print formatted
+format_attr(printf, 3, 4)
+inline char *seprintf(char *dst, char *end, const char *restrict fmt, ...);
+// vseprintf - va_list string end-pointer print formatted
+format_attr(printf, 3, 0)
+inline char *vseprintf(char *dst, char *end, const char *restrict fmt,
+    va_list ap);
+#endif
+
+
+#if !defined(HAVE_SEPRINTF)
+inline char *
+seprintf(char *dst, char *end, const char *restrict fmt, ...)
+{
+	char     *p;
+	va_list  ap;
+
+	va_start(ap, fmt);
+	p = vseprintf(dst, end, fmt, ap);
+	va_end(ap);
+
+	return p;
+}
+#endif
+
+
+#if !defined(HAVE_SEPRINTF)
+inline char *
+vseprintf(char *dst, char *end, const char *restrict fmt, va_list ap)
+{
+	int        len;
+	ptrdiff_t  size;
+
+	if (dst == NULL)
+		return NULL;
+
+	size = end - dst;
+	len = vstprintf(dst, size, fmt, ap);
+	if (len == -1)
+		return NULL;
+
+	return dst + len;
+}
+#endif
+
+
+#endif  // include guard
