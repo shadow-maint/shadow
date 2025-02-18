6c62103ba66220eac23dc89fb3764bf0f37d786f
Alejandro Colomar <alx@kernel.org>
2025-02-18 14:52:04 +0100

    lib/getdate.*: Reimplement in pure C
    This removes all yacc(1) code from this project.  Add copyright and
    license, since there remains nothing of the original code.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/getdate.c
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: 2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include <config.h>
+
+#include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
+
+#include "atoi/a2i/a2s.h"
+#include "getdate.h"
+#include "string/strchr/strchrcnt.h"
+#include "string/strcmp/streq.h"
+#include "string/strcmp/strprefix.h"
+#include "string/strcmp/strsuffix.h"
+#include "string/strspn/stpspn.h"
+
+
+#define TM_YEAR_ORIGIN 1900
+
+
+static long  yyDay;
+static long  yyMonth;
+static long  yyYear;
+
+
+static int parse_date(const char *s);
+
+
+time_t get_date (const char *p, const time_t *now)
+{
+  struct tm  tm;
+
+  if (parse_date(p) == -1)
+    return -1;
+
+  tm.tm_year = yyYear - TM_YEAR_ORIGIN;
+  tm.tm_mon = yyMonth - 1;
+  tm.tm_mday = yyDay;
+  tm.tm_hour = tm.tm_min = tm.tm_sec = 0;
+  tm.tm_isdst = 0;
+
+  return timegm(&tm);
+}
+
+
+static int
+parse_date(const char *s)
+{
+	long  n;
+
+	if (!streq(stpspn(s, "0123456789-"), "")
+	 || strchrcnt(s, '-') != 2
+	 || strprefix(s, "-")
+	 || strsuffix(s, "-")
+	 || strstr(s, "--"))
+	{
+		return -1;
+	}
+
+	if (a2sl(&yyYear, s, &s, 10, LONG_MIN, LONG_MAX) == -1 && errno != ENOTSUP)
+		return -1;
+
+	if (!strprefix(s++, "-"))
+		return -1;
+
+	if (a2sl(&yyMonth, s, &s, 10, LONG_MIN, LONG_MAX) == -1 && errno != ENOTSUP)
+		return -1;
+
+	if (!strprefix(s++, "-"))
+		return -1;
+
+	if (a2sl(&yyDay, s, NULL, 10, LONG_MIN, LONG_MAX) == -1)
+		return -1;
+
+	return 0;
+}
