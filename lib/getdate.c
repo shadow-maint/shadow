d4af3a81288e6c67626aa150f34c7df4a5d7e5bc
Alejandro Colomar <alx@kernel.org>
2025-02-18 23:25:29 +0100

    lib/getdate.c: Use strptime(3) to simplify
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/getdate.c
+++ lib/getdate.c
@@ -4,78 +4,29 @@
 
 #include <config.h>
 
-#include <errno.h>
-#include <limits.h>
 #include <stddef.h>
-#include <string.h>
+#include <strings.h>
 #include <time.h>
 
-#include "atoi/a2i/a2s.h"
 #include "getdate.h"
-#include "string/strchr/strchrcnt.h"
 #include "string/strcmp/streq.h"
-#include "string/strcmp/strprefix.h"
-#include "string/strcmp/strsuffix.h"
-#include "string/strspn/stpspn.h"
-
-
-#define TM_YEAR_ORIGIN 1900
-
-
-static long  yyDay;
-static long  yyMonth;
-static long  yyYear;
-
-
-static int parse_date(const char *s);
 
 
 time_t
-get_date(const char *s);
+get_date(const char *s)
 {
-	struct tm  tm;
+	struct tm   tm;
+	const char  *p;
 
-	if (parse_date(p) == -1)
+	bzero(&tm, sizeof(tm));
+
+	p = strptime("UTC", "%Z", &tm);
+	if (p == NULL || !streq(p, ""))
 		return -1;
 
-	tm.tm_year = yyYear - TM_YEAR_ORIGIN;
-	tm.tm_mon = yyMonth - 1;
-	tm.tm_mday = yyDay;
-	tm.tm_hour = tm.tm_min = tm.tm_sec = 0;
-	tm.tm_isdst = 0;
+	p = strptime(s, "%F", &tm);
+	if (p == NULL || !streq(p, ""))
+		return -1;
 
 	return timegm(&tm);
-}
-
-
-static int
-parse_date(const char *s)
-{
-	long  n;
-
-	if (!streq(stpspn(s, "0123456789-"), "")
-	 || strchrcnt(s, '-') != 2
-	 || strprefix(s, "-")
-	 || strsuffix(s, "-")
-	 || strstr(s, "--"))
-	{
-		return -1;
-	}
-
-	if (a2sl(&yyYear, s, &s, 10, LONG_MIN, LONG_MAX) == -1 && errno != ENOTSUP)
-		return -1;
-
-	if (!strprefix(s++, "-"))
-		return -1;
-
-	if (a2sl(&yyMonth, s, &s, 10, LONG_MIN, LONG_MAX) == -1 && errno != ENOTSUP)
-		return -1;
-
-	if (!strprefix(s++, "-"))
-		return -1;
-
-	if (a2sl(&yyDay, s, NULL, 10, LONG_MIN, LONG_MAX) == -1)
-		return -1;
-
-	return 0;
 }
