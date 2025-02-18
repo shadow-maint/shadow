fbd1489bd11c0762d63b783b1331d3d6d0870e5a
Alejandro Colomar <alx@kernel.org>
2025-02-18 15:38:35 +0100

    lib/: Move get_date() to lib/strtoday.c
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/strtoday.c
+++ lib/strtoday.c
@@ -1,21 +1,24 @@
-/*
- * SPDX-FileCopyrightText: 1991 - 1994, Julianne Frances Haugh
- * SPDX-FileCopyrightText: 1996 - 1999, Marek Michałkiewicz
- * SPDX-FileCopyrightText: 2003 - 2005, Tomasz Kłoczko
- * SPDX-FileCopyrightText: 2008       , Nicolas François
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+// SPDX-FileCopyrightText: 1991-1994, Julianne Frances Haugh
+// SPDX-FileCopyrightText: 1996-1999, Marek Michałkiewicz
+// SPDX-FileCopyrightText: 2003-2005, Tomasz Kłoczko
+// SPDX-FileCopyrightText: 2008, Nicolas François
+// SPDX-FileCopyrightText: 2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
 
 #include <config.h>
 
-#include <ctype.h>
+#include <stddef.h>
+#include <strings.h>
+#include <time.h>
 
 #include "atoi/str2i/str2s.h"
-#include "getdate.h"
+#include "defines.h"
 #include "prototypes.h"
 #include "string/strcmp/streq.h"
-#include "string/strspn/stpspn.h"
+
+
+static time_t get_date(const char *s);
 
 
 long
@@ -38,4 +41,24 @@ strtoday(const char *str)
 		return -2;
 	}
 	return t / DAY;
+}
+
+
+static time_t
+get_date(const char *s)
+{
+	struct tm   tm;
+	const char  *p;
+
+	bzero(&tm, sizeof(tm));
+
+	p = strptime("UTC", "%Z", &tm);
+	if (p == NULL || !streq(p, ""))
+		return -1;
+
+	p = strptime(s, "%F", &tm);
+	if (p == NULL || !streq(p, ""))
+		return -1;
+
+	return timegm(&tm);
 }
