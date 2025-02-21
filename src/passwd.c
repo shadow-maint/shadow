08742cf83cf0143d3a1be7bb650e11aa5a2a2b4f
Alejandro Colomar <alx@kernel.org>
2025-02-21 07:36:53 +0100

    lib/: Remove unused parameter $3 of password_check() and propagate
    Propagate the removal of dead code to its callers, which were only
    passing the parameter to this function.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/passwd.c
+++ src/passwd.c
@@ -320,7 +320,7 @@ static int new_password (const struct passwd *pw)
 				return -1;
 			}
 
-			if (!amroot && !obscure(orig, pass, pw)) {
+			if (!amroot && !obscure(orig, pass)) {
 				(void) puts (_("Try again."));
 				continue;
 			}
@@ -331,7 +331,7 @@ static int new_password (const struct passwd *pw)
 			 * --marekm
 			 */
 			if (amroot && !warned && getdef_bool ("PASS_ALWAYS_WARN")
-				&& !obscure(orig, pass, pw)) {
+				&& !obscure(orig, pass)) {
 				(void) puts (_("\nWarning: weak password (enter it again to use it anyway)."));
 				warned = true;
 				continue;
