0713070bf6ff9b5c881373da79777e922e709f61
Alejandro Colomar <alx@kernel.org>
2025-02-21 06:37:24 +0100

    lib/, src/: Remove unused parameter $2 of audit_logger()
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/cleanup_group.c
+++ lib/cleanup_group.c
@@ -27,7 +27,7 @@ void cleanup_report_add_group (void *group_name)
 
 	SYSLOG ((LOG_ERR, "failed to add group %s", name));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_ADD_GROUP, log_get_progname(),
+	audit_logger (AUDIT_ADD_GROUP,
 	              "",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -45,7 +45,7 @@ void cleanup_report_del_group (void *group_name)
 
 	SYSLOG ((LOG_ERR, "failed to remove group %s", name));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_DEL_GROUP, log_get_progname(),
+	audit_logger (AUDIT_DEL_GROUP,
 	              "",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -62,7 +62,7 @@ void cleanup_report_mod_group (void *cleanup_info)
 	         gr_dbname (),
 	         info->action));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_GRP_MGMT, log_get_progname(),
+	audit_logger (AUDIT_GRP_MGMT,
 	              info->audit_msg,
 	              info->name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -80,7 +80,7 @@ void cleanup_report_mod_gshadow (void *cleanup_info)
 	         sgr_dbname (),
 	         info->action));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_GRP_MGMT, log_get_progname(),
+	audit_logger (AUDIT_GRP_MGMT,
 	              info->audit_msg,
 	              info->name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -100,7 +100,7 @@ void cleanup_report_add_group_group (void *group_name)
 
 	SYSLOG ((LOG_ERR, "failed to add group %s to %s", name, gr_dbname ()));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_ADD_GROUP, log_get_progname(),
+	audit_logger (AUDIT_ADD_GROUP,
 	              "adding-group",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -120,7 +120,7 @@ void cleanup_report_add_group_gshadow (void *group_name)
 
 	SYSLOG ((LOG_ERR, "failed to add group %s to %s", name, sgr_dbname ()));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_GRP_MGMT, log_get_progname(),
+	audit_logger (AUDIT_GRP_MGMT,
 	              "adding-shadow-group",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -143,7 +143,7 @@ void cleanup_report_del_group_group (void *group_name)
 	         "failed to remove group %s from %s",
 	         name, gr_dbname ()));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_DEL_GROUP, log_get_progname(),
+	audit_logger (AUDIT_DEL_GROUP,
 	              "removing-group",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
@@ -166,7 +166,7 @@ void cleanup_report_del_group_gshadow (void *group_name)
 	         "failed to remove group %s from %s",
 	         name, sgr_dbname ()));
 #ifdef WITH_AUDIT
-	audit_logger (AUDIT_GRP_MGMT, log_get_progname(),
+	audit_logger (AUDIT_GRP_MGMT,
 	              "removing-shadow-group",
 	              name, AUDIT_NO_ID,
 	              SHADOW_AUDIT_FAILURE);
