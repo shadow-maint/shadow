94208c3eae44a04c9c5dcb518e89933f48bd2277
Alejandro Colomar <alx@kernel.org>
2025-02-21 08:05:42 +0100

    lib/, src/: Add [[gnu::unused]] to parameters used in conditionally-compiled code
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/idmapping.c
+++ lib/idmapping.c
@@ -20,6 +20,7 @@
 #include "alloc/calloc.h"
 #include "alloc/x/xmalloc.h"
 #include "atoi/a2i/a2u.h"
+#include "attr.h"
 #include "idmapping.h"
 #include "prototypes.h"
 #include "shadowlog.h"
@@ -121,7 +122,7 @@ static inline bool maps_lower_root(int cap, int ranges, const struct map_range *
  * If this is wanted: use file capabilities!
  */
 void write_mapping(int proc_dir_fd, int ranges, const struct map_range *mappings,
-	const char *map_file, uid_t ruid)
+	const char *map_file, MAYBE_UNUSED uid_t ruid)
 {
 	int idx;
 	const struct map_range *mapping;
