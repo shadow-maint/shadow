27d22d248020abc45b788f0e0c1fe7ceab2a9c95
Alejandro Colomar <alx@kernel.org>
2025-02-22 14:53:24 +0100

    lib/, src/: Use (intmax_t) to print a gid_t/uid_t
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/usermod.c
+++ src/usermod.c
@@ -27,6 +27,7 @@
 #endif				/* USE_PAM */
 #endif				/* ACCT_TOOLS_SETUID */
 #include <paths.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <strings.h>
 #include <sys/stat.h>
@@ -507,8 +508,8 @@ static void new_pwent (struct passwd *pwent)
 		              "changing-uid", user_newname, user_newid, 1);
 #endif
 		SYSLOG ((LOG_INFO,
-		         "change user '%s' UID from '%d' to '%d'",
-		         pwent->pw_name, pwent->pw_uid, user_newid));
+		         "change user '%s' UID from '%jd' to '%jd'",
+		         pwent->pw_name, (intmax_t) pwent->pw_uid, (intmax_t) user_newid));
 		pwent->pw_uid = user_newid;
 	}
 	if (gflg) {
@@ -518,8 +519,8 @@ static void new_pwent (struct passwd *pwent)
 		              user_newname, user_newid, 1);
 #endif
 		SYSLOG ((LOG_INFO,
-		         "change user '%s' GID from '%d' to '%d'",
-		         pwent->pw_name, pwent->pw_gid, user_newgid));
+		         "change user '%s' GID from '%jd' to '%jd'",
+		         pwent->pw_name, (intmax_t) pwent->pw_gid, (intmax_t) user_newgid));
 		pwent->pw_gid = user_newgid;
 	}
 	if (cflg) {
