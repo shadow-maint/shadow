7a1666323379354cf7834254e21a5de383956221
Alejandro Colomar <alx@kernel.org>
2025-02-23 00:22:13 +0100

    lib/commonio.c: Rely on the POSIX.1-2008 behavior of realpath(3)
    Link: <https://github.com/shadow-maint/shadow/pull/1222#discussion_r1966612238>
    Link: <https://stackoverflow.com/questions/60802732/are-realpath-portability-concerns-obsolete>
    Reported-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/commonio.c
+++ lib/commonio.c
@@ -74,17 +74,10 @@ int lrename (const char *old, const char *new)
 {
 	int res;
 	char *r = NULL;
-
-#ifndef __GLIBC__
-	char resolved_path[PATH_MAX];
-#endif				/* !__GLIBC__ */
 	struct stat sb;
+
 	if (lstat (new, &sb) == 0 && S_ISLNK (sb.st_mode)) {
-#ifdef __GLIBC__ /* now a POSIX.1-2008 feature */
 		r = realpath (new, NULL);
-#else				/* !__GLIBC__ */
-		r = realpath (new, resolved_path);
-#endif				/* !__GLIBC__ */
 		if (NULL == r) {
 			perror ("realpath in lrename()");
 		} else {
@@ -94,9 +87,7 @@ int lrename (const char *old, const char *new)
 
 	res = rename (old, new);
 
-#ifdef __GLIBC__
 	free (r);
-#endif				/* __GLIBC__ */
 
 	return res;
 }
