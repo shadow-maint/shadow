7924fdb94bb8d34cf9480914d77c4a514c643c59
Iker Pedrosa <ipedrosa@redhat.com>
2025-03-12 09:19:44 +0100

    tests/: extend basic groupdel tests
    Add additional check for gshadow entry.
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>
    Reviewed-by: Dan Lavu <dlavu@redhat.com>

--- tests/system/tests/test_groupdel.py
+++ tests/system/tests/test_groupdel.py
@@ -18,13 +18,18 @@ def test_groupdel__delete_group(shadow: Shadow):
         1. Create group
         2. Delete group
     :steps:
-        1. Group doesn't exist
+        1. Check group entry
+        2. Check gshadow entry
     :expectedresults:
-        1. Group is not found
+        1. group entry for the user doesn't exist
+        2. gshadow entry for the user doesn't exist
     :customerscenario: False
     """
     shadow.groupadd("tgroup")
     shadow.groupdel("tgroup")
 
     result = shadow.tools.getent.group("tgroup")
+    assert result is None, "Group should not be found"
+
+    result = shadow.tools.getent.gshadow("tgroup")
     assert result is None, "Group should not be found"
