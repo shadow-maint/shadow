9dcaaaa40d9b7ec25372d64b06e8c64c5aceff11
Alejandro Colomar <alx@kernel.org>
2025-03-12 21:45:33 +0100

    lib/search/cmp/: CMP(): Use a template to reduce repetition
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/cmp/cmp.c
+++ lib/search/cmp/cmp.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -7,7 +7,7 @@
 #include "search/cmp/cmp.h"
 
 
-extern inline int cmp_int(const void *key, const void *elt);
-extern inline int cmp_long(const void *key, const void *elt);
-extern inline int cmp_uint(const void *key, const void *elt);
-extern inline int cmp_ulong(const void *key, const void *elt);
+extern inline int CMP__int(const void *key, const void *elt);
+extern inline int CMP__long(const void *key, const void *elt);
+extern inline int CMP__u_int(const void *key, const void *elt);
+extern inline int CMP__u_long(const void *key, const void *elt);
