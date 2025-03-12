c70cf95f25d006fb04e4a387a8fb6b6ab0ef6abe
Alejandro Colomar <alx@kernel.org>
2025-03-12 22:37:13 +0100

    lib/search/l/: LFIND(): Use a template to add type safety
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/l/lfind.c
+++ lib/search/l/lfind.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -7,7 +7,14 @@
 #include "search/l/lfind.h"
 
 #include <stddef.h>
+#include <sys/types.h>
 
 
-extern inline const void *lfind_(const void *k, const void *a, size_t n, size_t ksize,
-    typeof(int (const void *k, const void *elt)) *cmp);
+extern inline const int *LFIND__int(size_t n;
+    const int *k, const int a[n], size_t n);
+extern inline const long *LFIND__long(size_t n;
+    const long *k, const long a[n], size_t n);
+extern inline const u_int *LFIND__u_int(size_t n;
+    const u_int *k, const u_int a[n], size_t n);
+extern inline const u_long *LFIND__u_long(size_t n;
+    const u_long *k, const u_long a[n], size_t n);
