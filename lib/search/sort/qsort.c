a65058138ae62502ea38b42bee1c30475d912849
Alejandro Colomar <alx@kernel.org>
2025-03-13 02:53:02 +0100

    lib/search/sort/: QSORT(): Use a template to add type safety
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/sort/qsort.c
+++ lib/search/sort/qsort.c
@@ -1,7 +1,20 @@
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
 #include "config.h"
 
 #include "search/sort/qsort.h"
+
+#include <stddef.h>
+#include <sys/types.h>
+
+
+extern inline void QSORT__int(size_t n;
+    int a[n], size_t n);
+extern inline void QSORT__long(size_t n;
+    long a[n], size_t n);
+extern inline void QSORT__u_int(size_t n;
+    u_int a[n], size_t n);
+extern inline void QSORT__u_long(size_t n;
+    u_long a[n], size_t n);
