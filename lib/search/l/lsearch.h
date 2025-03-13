f5902fabe99d50d82719b4e7695265f5bcbb88aa
Alejandro Colomar <alx@kernel.org>
2025-03-13 02:28:18 +0100

    lib/search/l/: LSEARCH(): Use a template to add type safety
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/l/lsearch.h
+++ lib/search/l/lsearch.h
@@ -9,17 +9,35 @@
 #include <config.h>
 
 #include <search.h>
+#include <stddef.h>
+#include <sys/types.h>
 
 #include "search/cmp/cmp.h"
 
 
-#define LSEARCH(T, k, a, n)  do                                       \
+#define LSEARCH(T, ...)                                               \
 (                                                                     \
-	const T  *k_ = k;                                             \
-	T        *a_ = a;                                             \
-                                                                      \
-	lsearch(k_, a_, n, sizeof(T), CMP(T));                        \
-} while (0)
+	_Generic((T) 0,                                               \
+		int:     LSEARCH__ ## int,                            \
+		long:    LSEARCH__ ## long,                           \
+		u_int:   LSEARCH__ ## u_int,                          \
+		u_long:  LSEARCH__ ## u_long                          \
+	)(__VA_ARGS__)                                                \
+)
+
+
+#define template_LSEARCH(T)                                           \
+inline void                                                           \
+LSEARCH__ ## T(size_t *n;                                             \
+    const T *k, T a[*n], size_t *n)                                   \
+{                                                                     \
+	lsearch(k, a, n, sizeof(T), CMP(T));                          \
+}
+template_LSEARCH(int);
+template_LSEARCH(long);
+template_LSEARCH(u_int);
+template_LSEARCH(u_long);
+#undef template_LSEARCH
 
 
 #endif  // include guard
