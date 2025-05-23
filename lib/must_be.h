94b1904b77c4f3b50fad6be57da38c99b0af2bda
Alejandro Colomar <alx@kernel.org>
2025-05-23 21:30:20 +0200

    autogen.sh, lib/: Replace must_be(is_array(a)) by -Werror=sizeof-pointer-div
    The error works as well as the magic macro, and we get cleaner code.
    Plus, very soon we'll get the countof() operator from GCC 16 and
    Clang 21, which doesn't even need this diagnostic to work safely.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/must_be.h
+++ lib/must_be.h
@@ -10,51 +10,6 @@
 
 #include <config.h>
 
-#include <assert.h>
-
-
-/*
- * SYNOPSIS
- *	int must_be(bool e);
- *
- * ARGUMENTS
- *	e	Expression to be asserted.
- *
- * DESCRIPTION
- *	This macro fails compilation if 'e' is false.  If 'e' is true,
- *	it returns (int) 0, so it doesn't affect the expression in which
- *	it is contained.
- *
- *	This macro is similar to static_assert(3).  While
- *	static_assert(3) can only be used where a statement is allowed,
- *	this must_be() macro can be used wherever an expression is
- *	allowed.
- *
- * RETURN VALUE
- *	0
- *
- * ERRORS
- *	If 'e' is false, the compilation will fail, as when using
- *	static_assert(3).
- *
- * EXAMPLES
- *	#define countof(a)  (sizeof(a) / sizeof(*(a)) + must_be(is_array(a)))
- *
- *	int foo[42];
- *	int bar[countof(foo)];
- */
-
-
-#define must_be(e)                                                            \
-(                                                                             \
-	0 * (int) sizeof(                                                     \
-		struct {                                                      \
-			static_assert(e, "");                                 \
-			int ISO_C_forbids_a_struct_with_no_members_;          \
-		}                                                             \
-	)                                                                     \
-)
-
 
 #define is_same_type(a, b)                                                    \
 (                                                                             \
@@ -65,12 +20,6 @@
 #define is_same_typeof(a, b)                                                  \
 (                                                                             \
 	is_same_type(typeof(a), typeof(b))                                    \
-)
-
-
-#define is_array(a)                                                           \
-(                                                                             \
-	!is_same_typeof(a, &(a)[0])                                           \
 )
 
 
