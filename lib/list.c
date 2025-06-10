28a0be813bf80815fd4e2406cfd85cbe4f8f1f14
Alejandro Colomar <alx@kernel.org>
2025-06-10 14:20:12 +0200

    lib/list.c: comma_to_list(): Use build_list() instead of its pattern
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/list.c
+++ lib/list.c
@@ -8,13 +8,15 @@
 
 #include "config.h"
 
+#include <stddef.h>
+#include <stdlib.h>
+
 #include "alloc/malloc.h"
 #include "prototypes.h"
 #include "defines.h"
 #include "string/strcmp/streq.h"
 #include "string/strdup/strdup.h"
 #include "string/strtok/astrsep2ls.h"
-#include "string/strtok/xastrsep2ls.h"
 
 #undef NDEBUG
 #include <assert.h>
@@ -198,7 +200,6 @@ comma_to_list(const char *comma)
 {
 	char *members;
 	char **array;
-	size_t  n;
 
 	assert (NULL != comma);
 
@@ -208,10 +209,10 @@ comma_to_list(const char *comma)
 
 	members = xstrdup (comma);
 
-	array = xastrsep2ls(members, ",", &n);
+	array = build_list(members);
+	if (array == NULL)
+		exit(EXIT_FAILURE);
 
-	if (streq(array[n-1], ""))
-		array[n-1] = NULL;
 	if (array[0] == NULL)
 		free(members);
 
