060629420f27d636d85d0f8a47c01eecd114ac10
Alejandro Colomar <alx@kernel.org>
2025-06-14 00:27:37 +0200

    lib/: Rename build_list() => acsv2ls()
    This name tells better what this API does.
    
    The name has a leading 'a', as it allocates memory, and then 'csv2ls'
    because it parses a CSV into a list.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/list.c
+++ lib/list.c
@@ -197,7 +197,7 @@ comma_to_list(const char *comma)
 
 	members = xstrdup (comma);
 
-	array = build_list(members);
+	array = acsv2ls(members);
 	if (array == NULL)
 		exit(EXIT_FAILURE);
 
@@ -209,7 +209,7 @@ comma_to_list(const char *comma)
 
 
 char **
-build_list(char *s)
+acsv2ls(char *s)
 {
 	char    **l;
 	size_t  n;
