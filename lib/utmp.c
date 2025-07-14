1a6a0ee41609196643c2eb595297d5eac5b5359f
Alejandro Colomar <alx@kernel.org>
2025-07-14 20:51:14 +0200

    lib/utmp.c: get_session_host(): Don't exit from library code
    This function already reports some errors, so it doesn't make sense to
    exit(3) on allocation error.  We can report it just like any other
    error.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/utmp.c
+++ lib/utmp.c
@@ -41,8 +41,6 @@
 #include "string/strdup/strdup.h"
 #include "string/strdup/strndup.h"
 
-#ident "$Id$"
-
 
 #define UTX_LINESIZE  countof(memberof(struct utmpx, ut_line))
 
@@ -209,7 +207,7 @@ get_session_host(pid_t main_pid)
 	ut = get_current_utmp(main_pid);
 
 	if ((ut != NULL) && !strneq_a(ut->ut_host, ""))
-		host = xstrndup_a(ut->ut_host);
+		host = strndup_a(ut->ut_host);
 	else
 		host = NULL;
 
