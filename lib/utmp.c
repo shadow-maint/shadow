ca7a891fb0ffeaffbdb2a2fef2c4476dc21d1878
Alejandro Colomar <alx@kernel.org>
2025-07-17 01:10:17 +0200

    lib/: Use STRNLEN() instead of its pattern
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/utmp.c
+++ lib/utmp.c
@@ -39,6 +39,7 @@
 #include "string/strdup/strndup.h"
 #include "string/strdup/xstrdup.h"
 #include "string/strdup/xstrndup.h"
+#include "string/strlen/strnlen.h"
 
 
 #define UTX_LINESIZE  countof(memberof(struct utmpx, ut_line))
@@ -298,8 +299,7 @@ prepare_utmp(const char *name, const char *line, const char *host,
 		struct addrinfo *info = NULL;
 		STRNCPY(utent->ut_host, hostname);
 #if defined(HAVE_STRUCT_UTMPX_UT_SYSLEN)
-		utent->ut_syslen = MIN (strlen (hostname),
-		                        countof(utent->ut_host));
+		utent->ut_syslen = STRNLEN(utent->ut_host);
 #endif
 #if defined(HAVE_STRUCT_UTMPX_UT_ADDR) || defined(HAVE_STRUCT_UTMPX_UT_ADDR_V6)
 		if (getaddrinfo (hostname, NULL, NULL, &info) == 0) {
