e65fe28da417ca913c50ebb3410c8e2955b8d52e
Alejandro Colomar <alx@kernel.org>
2025-07-17 01:10:17 +0200

    lib/: Use strnlen_a() instead of its pattern
    ut_syslen is the length of the nonstring stored in ut_host.  Let's
    measure it in the obvious way: strnlen(ut_host, countof(ut_host)), aka,
    strnlen_a(ut_host).
    
    MIN(strlen(hostname), countof(ut_host)) works because we've just copied
    hostname into ut_host, and the countof() field makes sure we take
    truncation into account.  But strnlen_a(ut_host) is just simpler, and
    more obviously correct.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/utmp.c
+++ lib/utmp.c
@@ -31,6 +31,7 @@
 #include "attr.h"
 #include "io/syslog.h"
 #include "memory/memcpy/strncpytail.h"
+#include "memory/strnlen/strnlen.h"
 #include "sizeof.h"
 #include "string/strchr/strnul.h"
 #include "string/strcmp/streq.h"
@@ -300,8 +301,7 @@ prepare_utmp(const char *name, const char *line, const char *host,
 		strncpy_a(utent->ut_host, hostname);
 #endif
 #if defined(HAVE_STRUCT_UTMPX_UT_SYSLEN)
-		utent->ut_syslen = MIN(strlen(hostname),
-		                       countof(utent->ut_host));
+		utent->ut_syslen = strnlen_a(utent->ut_host);
 #endif
 #if defined(HAVE_STRUCT_UTMPX_UT_ADDR) || defined(HAVE_STRUCT_UTMPX_UT_ADDR_V6)
 		if (getaddrinfo (hostname, NULL, NULL, &info) == 0) {
