c303977c1a1134cbd5b096230928643599745115
Alejandro Colomar <alx@kernel.org>
2025-07-22 22:14:08 +0200

    lib/string/: strerrno(): Add function
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strerrno.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include "config.h"
+
+#include "string/strerrno.h"
+
+
+extern inline char *strerrno(void);
