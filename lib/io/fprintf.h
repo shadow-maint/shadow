0ab8a82b50a24aee2883aedddda93c6bfc91e210
Alejandro Colomar <alx@kernel.org>
2025-07-23 14:19:52 +0200

    lib/io/fprintf.h: [v]fprintec(): Add functions
    These functions print a formatted string, followed by a colon and
    a space, followed by an error string produced by strerror($2).  That is,
    the output is as follows:
    
    	fmt: error message string
    
    For example,
    	fprintec(stderr, ENOMEM, "foo(%d)", 42);
    prints
    	foo(42): Cannot allocate memory
    
    These functions return the number of characters printed, or a negative
    value on error, like fprintf(3).
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/io/fprintf.h
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: 2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#ifndef SHADOW_INCLUDE_LIB_IO_FPRINTF_H_
+#define SHADOW_INCLUDE_LIB_IO_FPRINTF_H_
+
+
+#include "config.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "attr.h"
+
+
+format_attr(printf, 3, 4)
+inline int fprintec(FILE *restrict stream, int e,
+    const char *restrict fmt, ...);
+format_attr(printf, 3, 0)
+inline int vfprintec(FILE *restrict stream, int e,
+    const char *restrict fmt, va_list ap);
+
+
+// fprintec - FILE* print errno code
+inline int
+fprintec(FILE *restrict stream, int e, const char *restrict fmt, ...)
+{
+	int      len;
+	va_list  ap;
+
+	va_start(ap, fmt);
+	len = vfprintec(stream, e, fmt, ap);
+	va_end(ap);
+
+	return len;
+}
+
+
+// vfprintec - va_list FILE* print errno code
+inline int
+vfprintec(FILE *restrict stream, int e, const char *restrict fmt, va_list ap)
+{
+	int  l1, l2, l3;
+
+	l1 = vfprintf(stream, fmt, ap);
+	if (l1 < 0)
+		return l1;
+
+	l2 = fprintf(stream, l1 ? ": " : "");
+	if (l2 < 0)
+		return l2;
+
+	l3 = fprintf(stream, "%s\n", strerror(e));
+	if (l3 < 0)
+		return l3;
+
+	return l1 + l2 + l3;
+}
+
+
+#endif  // include guard
