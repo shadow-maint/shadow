e16e5bc91cf37b4445a6ca12e5aa60dae05b4b9a
Alejandro Colomar <alx@kernel.org>
2025-07-29 15:43:15 +0200

    lib/, src/: Use date_or_SDE() instead of its pattern
    The checks changing 0 into -1 were probably added because gettime()/DAY
    transformed a gettime() error code (-1) into 0, due to integer division.
    That was dead code: gettime() can't fail.
    
    It could also happen that the date is 0 (so the day is the Epoch), but
    this is unlikely, and anyway, I don't see any reasons to special-case
    that date.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/usermod.c
+++ src/usermod.c
@@ -614,14 +614,8 @@ static void new_spent (struct spwd *spent)
 	 */
 	spent->sp_pwdp = new_pw_passwd (spent->sp_pwdp);
 
-	if (pflg) {
-		spent->sp_lstchg = gettime () / DAY;
-		if (0 == spent->sp_lstchg) {
-			/* Better disable aging than requiring a password
-			 * change. */
-			spent->sp_lstchg = -1;
-		}
-	}
+	if (pflg)
+		spent->sp_lstchg = date_or_SDE();
 }
 
 /*
@@ -1754,12 +1748,7 @@ static void usr_update (void)
 			spent.sp_pwdp   = xstrdup (pwent.pw_passwd);
 			pwent.pw_passwd = xstrdup (SHADOW_PASSWD_STRING);
 
-			spent.sp_lstchg = gettime () / DAY;
-			if (0 == spent.sp_lstchg) {
-				/* Better disable aging than
-				 * requiring a password change */
-				spent.sp_lstchg = -1;
-			}
+			spent.sp_lstchg = date_or_SDE();
 			spent.sp_min    = getdef_num ("PASS_MIN_DAYS", -1);
 			spent.sp_max    = getdef_num ("PASS_MAX_DAYS", -1);
 			spent.sp_warn   = getdef_num ("PASS_WARN_AGE", -1);
