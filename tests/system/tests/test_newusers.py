de148f7746828c367404431256bf0ddb1cdd0325
Iker Pedrosa <ipedrosa@redhat.com>
2025-08-08 17:08:20 +0200

    tests/system/tests/test_newusers.py: create multiple users using file input
    This is the transformation to Python of the test located in
    `tests/newusers/20_multiple_users/newusers.test`, which checks that
    `newusers` is able to create new users from file.
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

--- tests/system/tests/test_newusers.py
+++ tests/system/tests/test_newusers.py
@@ -112,3 +112,104 @@ def test_newusers__create_users_from_stdin(shadow: Shadow):
         assert result.password == "*", "Incorrect password"
 
     assert shadow.fs.exists("/home/tuser2"), "tuser2 home folder should exist"
+
+
+@pytest.mark.topology(KnownTopology.Shadow)
+def test_newusers__create_users_from_file(shadow: Shadow):
+    """
+    :title: Create multiple users using file input
+    :setup:
+        1. Create temporary file with user data in passwd format
+        2. Create users using newusers with file input
+    :steps:
+        1. Check first user passwd entry
+        2. Check first user shadow entry
+        3. Check first user group entry
+        4. Check first user home folder
+        5. Check second user passwd entry
+        6. Check second user shadow entry
+        7. Check second user group entry
+        8. Check second user home folder
+    :expectedresults:
+        1. First user passwd entry exists with correct attributes
+        2. First user shadow entry exists with correct attributes
+        3. First user group entry exists with correct attributes
+        4. First user home folder exists
+        5. Second user passwd entry exists with correct attributes
+        6. Second user shadow entry exists with correct attributes
+        7. Second user group entry exists with correct attributes
+        8. Second user home folder exists
+    :customerscenario: False
+    """
+    temp_file = "/tmp/test_newusers_data.txt"
+    users_data = (
+        "tuser1:Secret123:1001:1001:Test User One:/home/tuser1:/bin/bash\n"
+        "tuser2:Secret123:1002:1002:Test User Two:/home/tuser2:/bin/bash"
+    )
+    shadow.fs.write(temp_file, users_data)
+
+    shadow.newusers(temp_file)
+
+    result = shadow.tools.getent.passwd("tuser1")
+    assert result is not None, "tuser1 user should be found in passwd"
+    assert result.name == "tuser1", "Incorrect username"
+    assert result.password == "x", "Incorrect password"
+    assert result.uid == 1001, "Incorrect UID"
+    assert result.gid == 1001, "Incorrect GID"
+    assert result.gecos == "Test User One", "Incorrect GECOS"
+    assert result.home == "/home/tuser1", "Incorrect home folder"
+    assert result.shell == "/bin/bash", "Incorrect shell"
+
+    result = shadow.tools.getent.shadow("tuser1")
+    assert result is not None, "tuser1 user should be found in shadow"
+    assert result.name == "tuser1", "Incorrect username"
+    assert result.password is not None, "Incorrect password"
+    assert result.last_changed == days_since_epoch(), "Incorrect last changed"
+    assert result.min_days == 0, "Incorrect min days"
+    assert result.max_days == 99999, "Incorrect max days"
+    assert result.warn_days == 7, "Incorrect warn days"
+
+    result = shadow.tools.getent.group("tuser1")
+    assert result is not None, "tuser1 group should be found"
+    assert result.name == "tuser1", "Incorrect group name"
+    assert result.gid == 1001, "Incorrect GID"
+
+    if shadow.host.features["gshadow"]:
+        result = shadow.tools.getent.gshadow("tuser1")
+        assert result is not None, "tuser1 group should be found"
+        assert result.name == "tuser1", "Incorrect group name"
+        assert result.password == "*", "Incorrect password"
+
+    assert shadow.fs.exists("/home/tuser1"), "tuser1 home folder should exist"
+
+    result = shadow.tools.getent.passwd("tuser2")
+    assert result is not None, "tuser2 user should be found in passwd"
+    assert result.name == "tuser2", "Incorrect username"
+    assert result.password == "x", "Incorrect password"
+    assert result.uid == 1002, "Incorrect UID"
+    assert result.gid == 1002, "Incorrect GID"
+    assert result.gecos == "Test User Two", "Incorrect GECOS"
+    assert result.home == "/home/tuser2", "Incorrect home folder"
+    assert result.shell == "/bin/bash", "Incorrect shell"
+
+    result = shadow.tools.getent.shadow("tuser2")
+    assert result is not None, "tuser2 user should be found in shadow"
+    assert result.name == "tuser2", "Incorrect username"
+    assert result.password is not None, "Incorrect password"
+    assert result.last_changed == days_since_epoch(), "Incorrect last changed"
+    assert result.min_days == 0, "Incorrect min days"
+    assert result.max_days == 99999, "Incorrect max days"
+    assert result.warn_days == 7, "Incorrect warn days"
+
+    result = shadow.tools.getent.group("tuser2")
+    assert result is not None, "tuser2 group should be found"
+    assert result.name == "tuser2", "Incorrect group name"
+    assert result.gid == 1002, "Incorrect GID"
+
+    if shadow.host.features["gshadow"]:
+        result = shadow.tools.getent.gshadow("tuser2")
+        assert result is not None, "tuser2 group should be found"
+        assert result.name == "tuser2", "Incorrect group name"
+        assert result.password == "*", "Incorrect password"
+
+    assert shadow.fs.exists("/home/tuser2"), "tuser2 home folder should exist"
