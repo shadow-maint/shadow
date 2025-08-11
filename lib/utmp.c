791e81b955aa66c2e364ee70db2e575575d30ec8
Alejandro Colomar <alx@kernel.org>
2025-08-11 22:39:10 +0200

    lib/utmp.c: get_current_utmp(): Move MEMDUP() to the return statement
    This removes the reuse of 'ut', with which I wasn't entirely happy.
    It also reduces the scope between setutxent()/endutxent().
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/utmp.c
+++ lib/utmp.c
@@ -195,11 +195,9 @@ get_current_utmp(pid_t main_pid)
 		}
 	}
 
-	ut = match ? MEMDUP(&ut_copy, struct utmpx) : NULL;
-
 	endutxent();
 
-	return ut;
+	return match ? MEMDUP(&ut_copy, struct utmpx) : NULL;
 }
 
 
