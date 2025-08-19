300c48550049bb83b2ca50482a68af69f13666e7
Iker Pedrosa <ipedrosa@redhat.com>
2025-08-19 11:23:16 +0200

    tests/system/framework/roles/shadow.py: implement binding for `groupmems`
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

--- tests/system/framework/roles/shadow.py
+++ tests/system/framework/roles/shadow.py
@@ -164,3 +164,25 @@ class Shadow(BaseLinuxRole[ShadowHost]):
         self.host.discard_file("/etc/gshadow")
 
         return cmd
+
+    def groupmems(self, *args, user: str = "root", password: str = None) -> ProcessResult:
+        """
+        Administer members of a user's primary group.
+
+        The groupmems command allows management of group membership lists.
+        If `user` is provided, then the `password` must be provided and the command groupmems run under this user.
+        If `user` isn't provided, then the command is run under `root`.
+        """
+        args_dict = self._parse_args(args)
+
+        if user == "root":
+            self.logger.info(f'Administer {args_dict["name"]} group membership as root on {self.host.hostname}')
+            cmd = self.host.conn.run("groupmems " + args[0], log_level=ProcessLogLevel.Error)
+        else:
+            self.logger.info(f'Administer {args_dict["name"]} group membership on {self.host.hostname}')
+            cmd = self.host.conn.run(f"echo '{password}' | su - {user} -c 'groupmems {args[0]}'", log_level=ProcessLogLevel.Error)
+
+        self.host.discard_file("/etc/group")
+        self.host.discard_file("/etc/gshadow")
+
+        return cmd
