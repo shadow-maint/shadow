d955317f2365d1b1d85ddfd8fa00df5facee6967
Anders Blomdell <anders.blomdell@control.lth.se>
2025-09-02 11:51:14 +0200

    When using nss-module, avoid using '/etc/sub[ug]id'
    Use 'want_sub[ug]id_file' to short-circuit acess to '/etc/sub[ug]id' when
    nss-moduleis active

--- src/usermod.c
+++ src/usermod.c
@@ -2185,8 +2185,8 @@ int main (int argc, char **argv)
 	is_shadow_grp = sgr_file_present ();
 #endif
 #ifdef ENABLE_SUBIDS
-	is_sub_uid = sub_uid_file_present ();
-	is_sub_gid = sub_gid_file_present ();
+	is_sub_uid = want_subuid_file() && sub_uid_file_present();
+	is_sub_gid = want_subgid_file() && sub_gid_file_present();
 #endif				/* ENABLE_SUBIDS */
 
 	process_flags (argc, argv);
