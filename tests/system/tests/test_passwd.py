e0f42a0b7a4e1a8693d29c2ab4c6c7d77ea3e410
Iker Pedrosa <ipedrosa@redhat.com>
2025-09-09 09:24:05 +0200

    tests/system/tests/test_passwd.py: change user password as user in interactive mode
    This is the transformation to Python of the test located in
    `tests/passwd/19_passwd_user_change_password/passwd.test`,
    which checks that `passwd` is able to change a user's password running
    as the user in interactive mode.
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

--- tests/system/tests/test_passwd.py
+++ tests/system/tests/test_passwd.py
@@ -69,3 +69,35 @@ def test_passwd__change_password_as_root_interactive(shadow: Shadow):
     assert shadow_entry.min_days == 0, "Incorrect min days"
     assert shadow_entry.max_days == 99999, "Incorrect max days"
     assert shadow_entry.warn_days == 7, "Incorrect warn days"
+
+
+@pytest.mark.topology(KnownTopology.Shadow)
+def test_passwd__change_password_as_user_interactive(shadow: Shadow):
+    """
+    :title: Change user password as user in interactive mode
+    :setup:
+        1. Create test user
+    :steps:
+        1. Change user password as root
+        2. Change user password as user in interactive mode
+        3. Check the user's shadow entry
+    :expectedresults:
+        1. User's password is changed as root
+        2. User's password is changed as user
+        3. User's shadow entry is correct
+    :customerscenario: False
+    """
+    shadow.useradd("tuser")
+
+    shadow.passwd("tuser", new_password="Secret123")
+    shadow.passwd("", run_as="tuser", old_password="Secret123", new_password="XcqKCymg")
+
+    shadow_entry = shadow.tools.getent.shadow("tuser")
+    assert shadow_entry is not None, "User should be found"
+    assert shadow_entry.name == "tuser", "Incorrect username"
+    assert shadow_entry.password is not None, "Password should not be None"
+    assert re.match(shadow_password_pattern(), shadow_entry.password), "Incorrect password"
+    assert shadow_entry.last_changed == days_since_epoch(), "Incorrect last changed"
+    assert shadow_entry.min_days == 0, "Incorrect min days"
+    assert shadow_entry.max_days == 99999, "Incorrect max days"
+    assert shadow_entry.warn_days == 7, "Incorrect warn days"
