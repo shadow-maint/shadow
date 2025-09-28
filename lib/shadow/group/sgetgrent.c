c66f24b568efaabb0963b7ff1608505cfba7c2c5
Alejandro Colomar <alx@kernel.org>
2025-09-28 18:58:24 +0200

    lib/shadow/: Add re-entrant functions
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/shadow/group/sgetgrent.c
+++ lib/shadow/group/sgetgrent.c
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 1996-1998, Marek Michałkiewicz
 // SPDX-FileCopyrightText: 2005, Tomasz Kłoczko
 // SPDX-FileCopyrightText: 2008, Nicolas François
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2025, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -10,6 +10,7 @@
 
 #include "shadow/group/sgetgrent.h"
 
+#include <errno.h>
 #include <grp.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -24,6 +25,7 @@
 #include "string/strcpy/stpecpy.h"
 #include "string/strtok/stpsep.h"
 #include "string/strtok/strsep2arr.h"
+#include "typetraits.h"
 
 
 // from-string get group entry
@@ -34,8 +36,7 @@ sgetgrent(const char *s)
 	static struct group  grent_ = {};
 	struct group         *grent = &grent_;
 
-	char    *fields[4];
-	char    *p, *end;
+	int     e;
 	size_t  n, lssize, size;
 
 	n = strchrcnt(s, ',') + 2;
@@ -47,24 +48,53 @@ sgetgrent(const char *s)
 	if (buf == NULL)
 		return NULL;
 
+	e = sgetgrent_r(s, grent, buf, size);
+	if (e != 0) {
+		errno = e;
+		return NULL;
+	}
+
+	return grent;
+}
+
+
+// from-string get group entry re-entrant
+int
+sgetgrent_r(size_t size;
+    const char *restrict s, struct group *restrict grent,
+    char buf[restrict size], size_t size)
+{
+	char    *p, *end;
+	char    *fields[4];
+	size_t  n, lssize;
+
+	// The first 'lssize' bytes of 'buf' are used for 'grent->gr_mem'.
+	n = strchrcnt(s, ',') + 2;
+	lssize = n * sizeof(char *);
+	if (lssize >= size)
+		return E2BIG;
+
+	// The remaining bytes of 'buf' are used for a copy of 's'.
 	end = buf + size;
 	p = buf + lssize;
 	if (stpecpy(p, end, s) == NULL)
-		return NULL;
+		return errno;
 
 	stpsep(p, "\n");
 
 	if (strsep2arr_a(p, ":", fields) == -1)
-		return NULL;
+		return EINVAL;
 
 	grent->gr_name = fields[0];
 	grent->gr_passwd = fields[1];
 	if (get_gid(fields[2], &grent->gr_gid) == -1)
-		return NULL;
+		return errno;
 
+	if (!is_aligned(buf, char *))
+		return EINVAL;
 	grent->gr_mem = (char **) buf;
 	if (csv2ls(fields[3], n, grent->gr_mem) == -1)
-		return NULL;
+		return errno;
 
-	return grent;
+	return 0;
 }
--- lib/shadow/gshadow/sgetsgent.c
+++ lib/shadow/gshadow/sgetsgent.c
@@ -10,6 +10,7 @@
 
 #include "shadow/gshadow/sgetsgent.h"
 
+#include <errno.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,6 +23,7 @@
 #include "string/strcpy/stpecpy.h"
 #include "string/strtok/stpsep.h"
 #include "string/strtok/strsep2arr.h"
+#include "typetraits.h"
 
 
 #if defined(SHADOWGRP) && !__has_include(<gshadow.h>)
@@ -33,9 +35,9 @@ sgetsgent(const char *s)
 	static struct sgrp  sgent_ = {};
 	struct sgrp         *sgent = &sgent_;
 
-	char    *fields[4];
-	char    *p, *end;
-	size_t  n, nadm, nmem, lssize, size;
+	int          e;
+	size_t       n, lssize, size;
+	struct sgrp  *dummy;
 
 	n = strchrcnt(s, ',') + 4;
 	lssize = n * sizeof(char *);  // For 'sgent->sg_adm' and 'sgent->sg_mem'
@@ -46,33 +48,66 @@ sgetsgent(const char *s)
 	if (buf == NULL)
 		return NULL;
 
+	e = sgetsgent_r(s, sgent, buf, size, &dummy);
+	if (e != 0) {
+		errno = e;
+		return NULL;
+	}
+
+	return sgent;
+}
+
+
+// from-string get shadow group entry re-entrant
+int
+sgetsgent_r(size_t size;
+    const char *s, struct sgrp *sgent, char buf[size], size_t size,
+    struct sgrp **dummy)
+{
+	char    *fields[4];
+	char    *p, *end;
+	size_t  n, nadm, nmem, lssize;
+
+	// 'dummy' exists only for historic reasons.
+	if (dummy != NULL)
+		*dummy = sgent;
+
+	// The first 'lssize' bytes of 'buf' are used for 'sg_adm' and 'sg_mem'.
+	n = strchrcnt(s, ',') + 4;
+	lssize = n * sizeof(char *);
+	if (lssize >= size)
+		return E2BIG;
+
+	// The remaining bytes of 'buf' are used for a copy of 's'.
 	end = buf + size;
 	p = buf + lssize;
 	if (stpecpy(p, end, s) == NULL)
-		return NULL;
+		return errno;
 
 	stpsep(p, "\n");
 
 	if (strsep2arr_a(p, ":", fields) == -1)
-		return NULL;
+		return EINVAL;
 
 	sgent->sg_namp = fields[0];
 	sgent->sg_passwd = fields[1];
 
+	if (!is_aligned(buf, char *))
+		return EINVAL;
 	sgent->sg_adm = (char **) buf;
 	nadm = strchrcnt(fields[2], ',') + 2;
 	if (nadm > n)
-		return NULL;
+		return E2BIG;
 	if (csv2ls(fields[2], nadm, sgent->sg_adm) == -1)
-		return NULL;
+		return errno;
 
 	sgent->sg_mem = sgent->sg_adm + nadm;
 	nmem = strchrcnt(fields[3], ',') + 2;
 	if (nmem + nadm > n)
-		return NULL;
+		return E2BIG;
 	if (csv2ls(fields[3], nmem, sgent->sg_mem) == -1)
-		return NULL;
+		return errno;
 
-	return sgent;
+	return 0;
 }
 #endif
