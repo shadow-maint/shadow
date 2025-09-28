fd9a0ff00c9236a66c62adefdbff0556fda63230
Alejandro Colomar <alx@kernel.org>
2025-09-28 18:58:24 +0200

    lib/shadow/: Add re-entrant functions
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/shadow/subid/sgetsient.c
+++ lib/shadow/subid/sgetsient.c
@@ -29,7 +29,7 @@ sgetsient(const char *s)
 	static struct subordinate_range  sient_ = {};
 	struct subordinate_range         *sient = &sient_;
 
-	char *fields[SUBID_NFIELDS];
+	int     e;
 	size_t  size;
 
 	size = strlen(s) + 1;
@@ -39,23 +39,41 @@ sgetsient(const char *s)
 	if (buf == NULL)
 		return NULL;
 
-	if (strtcpy(buf, s, size) == -1)
-		return NULL;
-
-	if (strsep2arr_a(buf, ":", fields) == -1)
-		return NULL;
-
-	if (streq(fields[0], ""))
-		return NULL;
-	sient->owner = fields[0];
-	if (a2ul(&sient->start, fields[1], NULL, 0, 0, maxof(id_t)) == -1)
-		return NULL;
-	if (a2ul(&sient->count, fields[2], NULL, 0, 0,
-	         MIN(maxof(id_t) + 1LL - sient->start, maxof(id_t))) == -1)
-	{
+	e = sgetsient_r(s, sient, buf, size);
+	if (e != 0) {
+		errno = e;
 		return NULL;
 	}
 
 	return sient;
+}
+
+
+// from-string get sub-ID entry re-entrant
+int
+sgetsient_r(size_t size;
+    const char *restrict s, struct subordinate_range *restrict sient,
+    char buf[restrict size], size_t size)
+{
+	char  *fields[SUBID_NFIELDS];
+
+	if (strtcpy(buf, s, size) == -1)
+		return errno;
+
+	if (strsep2arr_a(buf, ":", fields) == -1)
+		return EINVAL;
+
+	if (streq(fields[0], ""))
+		return EINVAL;
+	sient->owner = fields[0];
+	if (a2ul(&sient->start, fields[1], NULL, 0, 0, maxof(id_t)) == -1)
+		return errno;
+	if (a2ul(&sient->count, fields[2], NULL, 0, 0,
+	         MIN(maxof(id_t) + 1LL - sient->start, maxof(id_t))) == -1)
+	{
+		return errno;
+	}
+
+	return 0;
 }
 #endif
