8f066be87f4bd55e851d5a8d82f4a93cffb8df99
Alejandro Colomar <alx@kernel.org>
2025-09-29 03:05:00 +0200

    Revert "lib/shadow/: Use pointer to structure"
    That change was temporary to allow adding a re-entrant variant of the
    function with a small diff.  Remove it now that it's done.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/shadow/passwd/sgetpwent.c
+++ lib/shadow/passwd/sgetpwent.c
@@ -32,8 +32,7 @@ struct passwd *
 sgetpwent(const char *s)
 {
 	static char           *buf = NULL;
-	static struct passwd  pwent_ = {};
-	struct passswd        *pwent = &pwent_;
+	static struct passwd  pwent = {};
 
 	int     e;
 	size_t  size;
@@ -45,13 +44,13 @@ sgetpwent(const char *s)
 	if (buf == NULL)
 		return NULL;
 
-	e = sgetpwent_r(s, pwent, buf, size);
+	e = sgetpwent_r(s, &pwent, buf, size);
 	if (e != 0) {
 		errno = e;
 		return NULL;
 	}
 
-	return pwent;
+	return &pwent;
 }
 
 
