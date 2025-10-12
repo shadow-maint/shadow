c43da93642667ed1b4f701295305cc87110e25ac
Alejandro Colomar <alx@kernel.org>
2025-10-12 20:14:36 +0200

    lib/, src/: Use strcspn(3) instead of its pattern
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/useradd.c
+++ src/useradd.c
@@ -1380,16 +1380,13 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 				break;
 			case 's':
 				if (   ( !VALID (optarg) )
-				    || (   !streq(optarg, "")
-				        && ('/'  != optarg[0])
-				        && ('*'  != optarg[0]) )) {
+				    || strcspn(optarg, "/*")) {
 					fprintf (stderr,
 					         _("%s: invalid shell '%s'\n"),
 					         Prog, optarg);
 					exit (E_BAD_ARG);
 				}
-				if (!streq(optarg, "")
-				     && '*'  != optarg[0]
+				if (strcspn(optarg, "*")
 				     && !streq(optarg, "/sbin/nologin")
 				     && !streq(optarg, "/usr/sbin/nologin")
 				     && (   stat(optarg, &st) != 0
