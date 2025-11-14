a215b3b892b2040e6acf5b99fd3615f53db2680e
Alejandro Colomar <alx@kernel.org>
2025-11-14 01:50:11 +0100

    lib/alloc/: REALLOC[F](): Move _Generic(3) to separate line
    This should be more readable.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/alloc/realloc.h
+++ lib/alloc/realloc.h
@@ -17,7 +17,8 @@
 #define REALLOC(p, n, T)   REALLOC_(p, n, typeas(T))
 #define REALLOC_(p, n, T)                                             \
 ({                                                                    \
-	_Generic(p, T *: (T *){reallocarray_(p, n, sizeof(T))});      \
+	_Generic(p, T *: (void)0);                                    \
+	(T *){reallocarray_(p, n, sizeof(T))};                        \
 })
 
 #define reallocarray_(p, n, size)  reallocarray(p, (n) ?: 1, (size) ?: 1)
