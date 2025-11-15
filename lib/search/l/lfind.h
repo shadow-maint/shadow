948fb0bb76b9bb4269184094acd781547834446f
Alejandro Colomar <alx@kernel.org>
2025-11-15 21:42:56 +0100

    lib/search/: Split APIs
    Have xxx_T() macros that only add type safety to the function they wrap.
    
    Have XXX() macros that do more magic; in this case, they deduce the
    comparison function that is appropriate for the type.
    
    This will allow using the xxx_T() macros in more complex cases, where
    the function can't be deduced just from the type.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/l/lfind.h
+++ lib/search/l/lfind.h
@@ -14,12 +14,15 @@
 #include "search/cmp/cmp.h"
 
 
-#define LFIND(T, k, a, n)                                             \
+// lfind_T - linear find type-safe
+#define lfind_T(T, k, a, n, cmp)                                      \
 ({                                                                    \
 	_Generic(k, T *: 0, const T *: 0);                            \
 	_Generic(a, T *: 0, const T *: 0);                            \
-	(T *) lfind_(k, a, n, sizeof(T), CMP(T));                     \
+	(T *) lfind_(k, a, n, sizeof(T), cmp);                        \
 })
+
+#define LFIND(T, ...)  lfind_T(T, __VA_ARGS__, CMP(T))
 
 
 inline void *lfind_(const void *k, const void *a, size_t n, size_t ksize,
