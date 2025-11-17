e6d4df2b045bbc984385d42a14c4d225bdd30fe9
Alejandro Colomar <alx@kernel.org>
2025-11-17 20:50:31 +0100

    lib/search/: lsearch_T(): Don't return anything
    We don't use this value.  This silences a diagnostic about the unused
    return value.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/search/l/lsearch.h
+++ lib/search/l/lsearch.h
@@ -16,10 +16,10 @@
 
 // lsearch_T - linear search-and-insert type-safe
 #define lsearch_T(T, ...)            lsearch_T_(typeas(T), __VA_ARGS__)
-#define lsearch_T_(T, k, a, n, cmp)                                   \
-(                                                                     \
-	(T *){lsearch((const T *){k}, (T *){a}, n, sizeof(T), cmp)}   \
-)
+#define lsearch_T_(T, k, a, n, cmp)  do                               \
+{                                                                     \
+	lsearch((const T *){k}, (T *){a}, n, sizeof(T), cmp);         \
+} while (0)
 
 #define LSEARCH(T, ...)  lsearch_T(T, __VA_ARGS__, CMP(T))
 
