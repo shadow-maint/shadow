c076db404d04ac15c2e2cdd0b74af4cafea4ed18
Alejandro Colomar <alx@kernel.org>
2025-11-19 18:16:39 +0100

    lib/, src/: Use default argument values in a2i() calls
    This removes chances of typos.  It would be possible to accidentally
    pass for example ULONG_MAX or INT_MAX where LONG_MAX would be
    appropriate.  Because these macros are able to automate the right value,
    don't specify it manually.
    
    Thus, when we specify it manually, it will be in those places where we
    use a non-default value, which is less common.  That will make it easier
    to review those few places.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/shadow/shadow/sgetspent.c
+++ lib/shadow/shadow/sgetspent.c
@@ -72,7 +72,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[2], ""))
 		spwd.sp_lstchg = -1;
-	else if (a2sl(&spwd.sp_lstchg, fields[2], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_lstchg, fields[2], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
@@ -81,7 +81,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[3], ""))
 		spwd.sp_min = -1;
-	else if (a2sl(&spwd.sp_min, fields[3], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_min, fields[3], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
@@ -90,7 +90,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[4], ""))
 		spwd.sp_max = -1;
-	else if (a2sl(&spwd.sp_max, fields[4], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_max, fields[4], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
@@ -113,7 +113,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[5], ""))
 		spwd.sp_warn = -1;
-	else if (a2sl(&spwd.sp_warn, fields[5], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_warn, fields[5], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
@@ -123,7 +123,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[6], ""))
 		spwd.sp_inact = -1;
-	else if (a2sl(&spwd.sp_inact, fields[6], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_inact, fields[6], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
@@ -133,7 +133,7 @@ sgetspent(const char *s)
 
 	if (streq(fields[7], ""))
 		spwd.sp_expire = -1;
-	else if (a2sl(&spwd.sp_expire, fields[7], NULL, 0, 0, LONG_MAX) == -1)
+	else if (a2sl(&spwd.sp_expire, fields[7], NULL, 0, 0,) == -1)
 		return NULL;
 
 	/*
