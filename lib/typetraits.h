c256406654051a417a93c1eb0b298905f3ea1566
Alejandro Colomar <alx@kernel.org>
2025-11-27 01:06:13 +0100

    lib/typetraits.h: Compact macros
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/typetraits.h
+++ lib/typetraits.h
@@ -11,36 +11,12 @@
 #include "sizeof.h"
 
 
-#define is_unsigned(x)                                                        \
-(                                                                             \
-	(typeof(x)) -1 > 1                                                    \
-)
-
-#define is_signed(x)                                                          \
-(                                                                             \
-	(typeof(x)) -1 < 1                                                    \
-)
-
-
-#define stype_max(T)                                                          \
-(                                                                             \
-	(T) (((((T) 1 << (WIDTHOF(T) - 2)) - 1) << 1) + 1)                    \
-)
-
-#define utype_max(T)                                                          \
-(                                                                             \
-	(T) -1                                                                \
-)
-
-#define type_max(T)                                                           \
-(                                                                             \
-	(T) (is_signed(T) ? stype_max(T) : utype_max(T))                      \
-)
-
-#define type_min(T)                                                           \
-(                                                                             \
-	(T) ~type_max(T)                                                      \
-)
+# define is_unsigned(x)  ((typeof(x)) -1 > 1)
+# define is_signed(x)    ((typeof(x)) -1 < 1)
+# define stype_max(T)    ((T) (((((T) 1 << (WIDTHOF(T) - 2)) - 1) << 1) + 1))
+# define utype_max(T)    ((T) -1)
+# define type_max(T)     ((T) (is_signed(T) ? stype_max(T) : utype_max(T)))
+# define type_min(T)     ((T) ~type_max(T))
 
 
 #define is_same_type(a, b)                                                    \
