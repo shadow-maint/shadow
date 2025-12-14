bcb9bd4aac24b673f19ab156a132c2fce5b750f5
Alejandro Colomar <alx@kernel.org>
2025-12-14 16:47:20 +0100

    lib/string/strcpy/: strmove(): Add function
    While we could copy the terminating null byte as part of the memmove(3)
    call, by doing strlen(src)+1, let's copy the terminating null byte
    explicitly.
    
    This needs using mempmove_T(), but it's more robust, since it makes this
    API thread-safe.  If the string changes between strlen(3) and
    memmove(3), we still write a terminating null byte, which makes sure
    this remains being a string, preventing serious vulnerabilities.
    
    We don't need that, since our programs are single-threaded, but it
    doesn't hurt, and if some other project copies this API from us, it's
    better that they have it thread-safe, just in case.
    
    Also, this removes the +1, which is slightly prone to off-by-one bugs.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

---
+++ lib/string/strcpy/strmove.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025-2026, Alejandro Colomar <alx@kernel.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#include "config.h"
+
+#include "string/strcpy/strmove.h"
+
+
+extern inline void strmove(char *dst, char *src);
