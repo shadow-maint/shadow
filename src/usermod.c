12fae34f5336de8d5a981f3d6d6fba6feb10126f
Alejandro Colomar <alx@kernel.org>
2025-12-16 21:23:19 +0100

    *: login.defs(5): PASS_MIN_DAYS: Remove configuration variable
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/usermod.c
+++ src/usermod.c
@@ -1705,7 +1705,7 @@ static void usr_update(const struct option_flags *flags)
 				 * requiring a password change */
 				spent.sp_lstchg = -1;
 			}
-			spent.sp_min    = getdef_num ("PASS_MIN_DAYS", -1);
+			spent.sp_min    = -1;
 			spent.sp_max    = getdef_num ("PASS_MAX_DAYS", -1);
 			spent.sp_warn   = getdef_num ("PASS_WARN_AGE", -1);
 			spent.sp_inact  = -1;
