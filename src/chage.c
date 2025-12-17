fd65423a6043716b378770f58476c2b14b3e38bf
Alejandro Colomar <alx@kernel.org>
2025-12-17 13:05:47 +0100

    *: chage(1): -m,--mindays: Remove option
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/chage.c
+++ src/chage.c
@@ -57,7 +57,6 @@ static bool
     Eflg = false,		/* set account expiration date */
     iflg = false,		/* set iso8601 date formatting */
     lflg = false,		/* show account aging information */
-    mflg = false,		/* set minimum number of days before password change */
     Mflg = false,		/* set maximum number of days before password change */
     Wflg = false;		/* set expiration warning days */
 static bool amroot = false;
@@ -140,8 +139,6 @@ usage (int status)
 	(void) fputs (_("  -h, --help                    display this help message and exit\n"), usageout);
 	(void) fputs (_("  -i, --iso8601                 use YYYY-MM-DD when printing dates\n"), usageout);
 	(void) fputs (_("  -l, --list                    show account aging information\n"), usageout);
-	(void) fputs (_("  -m, --mindays MIN_DAYS        set minimum number of days before password\n"
-	                "                                change to MIN_DAYS\n"), usageout);
 	(void) fputs (_("  -M, --maxdays MAX_DAYS        set maximum number of days before password\n"
 	                "                                change to MAX_DAYS\n"), usageout);
 	(void) fputs (_("  -R, --root CHROOT_DIR         directory to chroot into\n"), usageout);
@@ -334,7 +331,6 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 		{"expiredate", required_argument, NULL, 'E'},
 		{"help",       no_argument,       NULL, 'h'},
 		{"list",       no_argument,       NULL, 'l'},
-		{"mindays",    required_argument, NULL, 'm'},
 		{"maxdays",    required_argument, NULL, 'M'},
 		{"root",       required_argument, NULL, 'R'},
 		{"prefix",     required_argument, NULL, 'P'},
@@ -374,15 +370,6 @@ static void process_flags (int argc, char **argv, struct option_flags *flags)
 			break;
 		case 'l':
 			lflg = true;
-			break;
-		case 'm':
-			mflg = true;
-			if (a2sl(&mindays, optarg, NULL, 0, -1, LONG_MAX) == -1) {
-				fprintf (stderr,
-				         _("%s: invalid numeric argument '%s'\n"),
-				         Prog, optarg);
-				usage (E_USAGE);
-			}
 			break;
 		case 'M':
 			Mflg = true;
@@ -432,7 +419,7 @@ static void check_flags (int argc, int opt_index)
 		usage (E_USAGE);
 	}
 
-	if (lflg && (mflg || Mflg || dflg || Wflg || Eflg)) {
+	if (lflg && (Mflg || dflg || Wflg || Eflg)) {
 		fprintf (stderr,
 		         _("%s: do not include \"l\" with other flags\n"),
 		         Prog);
@@ -634,9 +621,7 @@ static void get_defaults (/*@null@*/const struct spwd *sp)
 		if (!Mflg) {
 			maxdays = sp->sp_max;
 		}
-		if (!mflg) {
-			mindays = sp->sp_min;
-		}
+		mindays = sp->sp_min;
 		if (!dflg) {
 			lstchgdate = sp->sp_lstchg;
 		}
@@ -655,9 +640,7 @@ static void get_defaults (/*@null@*/const struct spwd *sp)
 		if (!Mflg) {
 			maxdays = -1;
 		}
-		if (!mflg) {
-			mindays = -1;
-		}
+		mindays = -1;
 		if (!dflg) {
 			lstchgdate = -1;
 		}
@@ -682,7 +665,6 @@ static void get_defaults (/*@null@*/const struct spwd *sp)
  *	-E	set account expiration date (*)
  *	-l	show account aging information
  *	-M	set maximum number of days before password change (*)
- *	-m	set minimum number of days before password change (*)
  *	-W	set expiration warning days (*)
  *
  *	(*) requires root permission to execute.
@@ -787,7 +769,7 @@ int main (int argc, char **argv)
 	 * If none of the fields were changed from the command line, let the
 	 * user interactively change them.
 	 */
-	if (!mflg && !Mflg && !dflg && !Wflg && !Eflg) {
+	if (!Mflg && !dflg && !Wflg && !Eflg) {
 		printf (_("Changing the aging information for %s\n"),
 		        user_name);
 		if (new_fields () == 0) {
@@ -807,10 +789,6 @@ int main (int argc, char **argv)
 		if (Mflg) {
 			audit_logger (AUDIT_USER_MGMT,
 			              "change-max-age", user_name, user_uid, SHADOW_AUDIT_SUCCESS);
-		}
-		if (mflg) {
-			audit_logger (AUDIT_USER_MGMT,
-			              "change-min-age", user_name, user_uid, 1);
 		}
 		if (dflg) {
 			audit_logger (AUDIT_USER_MGMT,
