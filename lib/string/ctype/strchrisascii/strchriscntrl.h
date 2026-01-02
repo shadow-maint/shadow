a3f1f857c101ba39f8798e9387d01cc3201c42f2
Alejandro Colomar <alx@kernel.org>
2026-01-02 21:29:27 +0100

    lib/, src/: Use isascii_c() functions instead of isascii(3)
    We want to use the C locale.
    
    Reported-by: Tobias Stoeckmann <tobias@stoeckmann.org>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/string/ctype/strchrisascii/strchriscntrl.h
+++ lib/string/ctype/strchrisascii/strchriscntrl.h
@@ -8,27 +8,23 @@
 
 #include "config.h"
 
-#include <ctype.h>
 #include <stdbool.h>
 
+#include "string/ctype/isascii.h"
 #include "string/strcmp/streq.h"
 
 
 inline bool strchriscntrl(const char *s);
 
 
-// string character is [:cntrl:]
-// Return true if any control character is found in the string.
-// Check both C0 (0x00-0x1F, 0x7F) via iscntrl(3) and C1 (0x80-0x9F)
-// explicitly, since glibc's iscntrl() does not classify C1 bytes as
-// control characters in any locale.
+// strchriscntrl - string character is control-character (C0 or C1)
 inline bool
 strchriscntrl(const char *s)
 {
 	for (; !streq(s, ""); s++) {
 		unsigned char  c = *s;
 
-		if (iscntrl(c))
+		if (iscntrl_c(c))
 			return true;
 		if (c >= 0x80 && c <= 0x9F)
 			return true;
