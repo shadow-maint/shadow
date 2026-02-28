fbdd1bbcb8cb1a1f622207f69fd33a9e7d54e61e
Alejandro Colomar <alx@kernel.org>
2026-02-28 21:37:26 +0100

    src/useradd.c: create_home(): Handle first slash separately
    This removes the only use of 'bhome' within the loop other than in the
    strtok(3) calls.  That was problematic, because it didn't allow changing
    this code to use strsep(3) --as strsep(3) wouldn't respect the first
    slash in the 'bhome' string as strtok(3) does--.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/useradd.c
+++ src/useradd.c
@@ -69,6 +69,7 @@
 #include "string/strcmp/strprefix.h"
 #include "string/strdup/strdup.h"
 #include "string/strerrno.h"
+#include "string/strspn/stpspn.h"
 #include "string/strtok/stpsep.h"
 #include "sysconf.h"
 
@@ -2248,9 +2249,10 @@ static void create_home(const struct option_flags *flags)
 	   owner root:root.
 	 */
 	strcpy(path, "");
+	if (strspn(bhome, "/"))
+		strcat(path, "/");
 	for (cp = strtok(bhome, "/"); cp != NULL; cp = strtok(NULL, "/")) {
-		/* Avoid turning a relative path into an absolute path. */
-		if (strprefix(bhome, "/") || !streq(path, ""))
+		if (!streq(stpspn(path, "/"), ""))
 			strcat(path, "/");
 
 		strcat(path, cp);
