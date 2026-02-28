a1a4f05fc20b37dda154d82fbbd033e16ca652cd
Alejandro Colomar <alx@kernel.org>
2026-02-28 21:46:49 +0100

    src/useradd.c: Use strsep(3) instead of strtok(3)
    This removes the remaining uses of strtok(3).
    
    strsep(3) differs from strtok(3) in that it doesn't collapse adjacent
    delimiters, and thus it produces empty strings for adjacent delimiters
    (slashes).  We must skip these to keep the strtok(3) behavior.  It makes
    sense, as we don't want to have unnecessarily repeated slashes in a
    path.  That's why we do
    
    	if (streq(cp, ""))
    		continue;
    
    Also, while strtok(3) holds internal state, strsep(3) doesn't, which
    means we hold the state externally.  Thus, we need to add the new
    pointer 'p'.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/useradd.c
+++ src/useradd.c
@@ -2142,7 +2142,7 @@ usr_update (unsigned long subuid_count, unsigned long subgid_count,
 static void create_home(const struct option_flags *flags)
 {
 	char    path[strlen(prefix_user_home) + 2];
-	char    *bhome, *cp;
+	char    *bhome, *cp, *p;
 	mode_t  mode;
 	bool    process_selinux;
 
@@ -2175,8 +2175,12 @@ static void create_home(const struct option_flags *flags)
 	strcpy(path, "");
 	if (strspn(bhome, "/"))
 		strcat(path, "/");
-	for (cp = strtok(bhome, "/"); cp != NULL; cp = strtok(NULL, "/")) {
+	p = bhome;
+	while (NULL != (cp = strsep(&p, "/"))) {
 		bool  dir_created;
+
+		if (streq(cp, ""))
+			continue;
 
 		if (!streq(stpspn(path, "/"), ""))
 			strcat(path, "/");
