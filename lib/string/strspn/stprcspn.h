913a991b018532a084ed397d1d51058d80e284ad
Alejandro Colomar <alx@kernel.org>
2026-03-01 22:27:33 +0100

    lib/string/strspn/: strr[c]spn(): Return the length counting from the end
    Instead of returning the offset from the start of the string, return the
    length of the span counting from the end.
    
    This makes the name of the function more representative of its behavior,
    and also makes the functions more useful: one can use them as booleans
    to determine whether a string ends in a set of characters or not.
    
    	if (strrspn(s, "xyz"))  // Does 's' end in any chars of "xyz"?
    
    Currently, the only uses of strr[c]spn() are for implementing
    stpr[c]spn(), so we had to update those too.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/string/strspn/stprcspn.h
+++ lib/string/strspn/stprcspn.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2024-2026, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
@@ -10,15 +10,16 @@
 
 #include <string.h>
 
+#include "string/strchr/strnul.h"
 #include "string/strspn/strrcspn.h"
 
 
-// string returns-pointer rear complement substring prefix length
-#define stprcspn(s, reject)                                                   \
-({                                                                            \
-	__auto_type  s_ = (s);                                                \
-	                                                                      \
-	s_ + strrcspn(s_, reject);                                            \
+// stprcspn - string returns-pointer rear complement span
+#define stprcspn(s, reject)                                           \
+({                                                                    \
+	__auto_type  s_ = (s);                                        \
+	                                                              \
+	strnul(s_) - strrcspn(s_, reject);                            \
 })
 
 
