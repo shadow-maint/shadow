6739a4edcf2a8db79c09f63e54d55cad35906e9f
Alejandro Colomar <alx@kernel.org>
2026-03-08 15:10:05 +0100

    lib/typetraits.h: VQChar_of(): Add macro for volatile-generic macros
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/typetraits.h
+++ lib/typetraits.h
@@ -45,6 +45,7 @@
 	)                                                             \
 )
 #define CQChar_of(s)    QChar_of(const, s)
+#define VQChar_of(s)    QChar_of(volatile, s)
 
 
 #endif  // include guard
