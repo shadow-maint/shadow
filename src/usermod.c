170626affc8c0b44d354240057b4e4f9c9183b77
Alejandro Colomar <alx@kernel.org>
2026-03-17 03:35:15 +0100

    src/usermod.c: open_files(): Refactor conditionals
    This prepares for the following commit.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/usermod.c
+++ src/usermod.c
@@ -1577,14 +1577,16 @@ static void close_files(const struct option_flags *flags)
 static void
 open_files(bool process_selinux)
 {
-	if (pw_lock() == 0) {
-		eprintf(_("%s: cannot lock %s; try again later.\n"), Prog, pw_dbname());
-		fail_exit(E_PW_UPDATE, process_selinux);
-	}
-	lk.pw = true;
-	if (pw_open(O_CREAT | O_RDWR) == 0) {
-		eprintf(_("%s: cannot open %s\n"), Prog, pw_dbname());
-		fail_exit(E_PW_UPDATE, process_selinux);
+	if (true) {
+		if (pw_lock() == 0) {
+			eprintf(_("%s: cannot lock %s; try again later.\n"), Prog, pw_dbname());
+			fail_exit(E_PW_UPDATE, process_selinux);
+		}
+		lk.pw = true;
+		if (pw_open(O_CREAT | O_RDWR) == 0) {
+			eprintf(_("%s: cannot open %s\n"), Prog, pw_dbname());
+			fail_exit(E_PW_UPDATE, process_selinux);
+		}
 	}
 	if (is_shadow_pwd && (lflg || pflg || eflg || fflg || Lflg || Uflg)) {
 		if (spw_lock() == 0) {
@@ -1597,7 +1599,6 @@ open_files(bool process_selinux)
 			fail_exit(E_PW_UPDATE, process_selinux);
 		}
 	}
-
 	if (Gflg || lflg) {
 		/*
 		 * Lock and open the group file. This will load all of the
@@ -1612,20 +1613,20 @@ open_files(bool process_selinux)
 			eprintf(_("%s: cannot open %s\n"), Prog, gr_dbname());
 			fail_exit(E_GRP_UPDATE, process_selinux);
 		}
-#ifdef SHADOWGRP
-		if (is_shadow_grp) {
-			if (sgr_lock() == 0) {
-				eprintf(_("%s: cannot lock %s; try again later.\n"), Prog, sgr_dbname());
-				fail_exit(E_GRP_UPDATE, process_selinux);
-			}
-			lk.sgr = true;
-			if (sgr_open(O_CREAT | O_RDWR) == 0) {
-				eprintf(_("%s: cannot open %s\n"), Prog, sgr_dbname());
-				fail_exit(E_GRP_UPDATE, process_selinux);
-			}
-		}
-#endif
 	}
+#ifdef SHADOWGRP
+	if ((Gflg || lflg) && is_shadow_grp) {
+		if (sgr_lock() == 0) {
+			eprintf(_("%s: cannot lock %s; try again later.\n"), Prog, sgr_dbname());
+			fail_exit(E_GRP_UPDATE, process_selinux);
+		}
+		lk.sgr = true;
+		if (sgr_open(O_CREAT | O_RDWR) == 0) {
+			eprintf(_("%s: cannot open %s\n"), Prog, sgr_dbname());
+			fail_exit(E_GRP_UPDATE, process_selinux);
+		}
+	}
+#endif
 #ifdef ENABLE_SUBIDS
 	if (vflg || Vflg) {
 		if (sub_uid_lock() == 0) {
@@ -1649,7 +1650,7 @@ open_files(bool process_selinux)
 			fail_exit(E_SUB_GID_UPDATE, process_selinux);
 		}
 	}
-#endif				/* ENABLE_SUBIDS */
+#endif
 }
 
 /*
