942decabcfbc056fa255132579fdd655fc3dafb2
Alejandro Colomar <alx@kernel.org>
2026-06-14 01:55:22 +0200

    src/login_nopam.c: from_match(): Unify style of conditionals
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/login_nopam.c
+++ src/login_nopam.c
@@ -309,16 +309,14 @@ static bool from_match (char *tok, const char *string)
 
 		str_len = strlen (string);
 		tok_len = strlen (tok);
-		if (   (str_len > tok_len)
-		    && strcaseeq(tok, string + str_len - tok_len)) {
+		if (str_len > tok_len && strcaseeq(tok, string + str_len - tok_len))
 			return true;
-		}
 	} else if (strcaseeq(tok, "LOCAL")) {	/* LOCAL: no dots */
 		if (!strchr(string, '.'))
 			return true;
-	} else if (   strrspn_(tok, ".")  /* network */
-		   && strprefix(resolve_hostname(string), tok)) {
-		return true;
+	} else if (strrspn_(tok, ".")  /* network */
+		if (strprefix(resolve_hostname(string), tok))
+			return true;
 	}
 	return false;
 }
