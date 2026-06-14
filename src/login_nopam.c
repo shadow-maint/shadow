fb5631253b06fd112934135bf7374ccebee42d79
Alejandro Colomar <alx@kernel.org>
2026-06-14 15:09:14 +0200

    src/login_nopam.c: login_access(): Move error handling earlier
    This will allow reducing indentation for the rest of the code.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/login_nopam.c
+++ src/login_nopam.c
@@ -92,11 +92,15 @@ login_access(const char *user, const char *from)
 	 * Process the table one line at a time and stop at the first match.
 	 * Blank lines and lines that begin with a '#' character are ignored.
 	 * Non-comment lines are broken at the ':' character. All fields are
-	 * mandatory. The first field should be a "+" or "-" character. A
-	 * non-existing table means no access control.
+	 * mandatory. The first field should be a "+" or "-" character.
 	 */
 	fp = fopen (TABLE, "r");
-	if (NULL != fp) {
+	if (fp == NULL) {
+		if (errno != ENOENT)
+			SYSLOGE(LOG_ERR, "cannot open %s", TABLE);
+		return true;  // A non-existent table means no access control.
+	}
+	{
 		intmax_t lineno = 0;	/* for diagnostics */
 		while (   !match
 		       && (fgets_a(line, fp) != NULL))
@@ -138,8 +142,6 @@ login_access(const char *user, const char *from)
 			         && list_match (users, user, user_match));
 		}
 		(void) fclose (fp);
-	} else if (errno != ENOENT) {
-		SYSLOGE(LOG_ERR, "cannot open %s", TABLE);
 	}
 	return (!match || strprefix(line, "+"))?1:0;
 }
