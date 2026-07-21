cbb8850534b22f8266af66e9b914f7f5ede2432e
Akshay Sakure <asakure@redhat.com>
2026-07-21 23:27:33 +0530

    Tests: Groupadd command fails when two groupnames are mentioned
    This is Python transformation of the test located in
    `tests/grouptools/groupadd/24_groupadd_2_groups/groupadd.test`
    which checks that `groupadd` command fails when two groupnames
    are mentioned
    
    Signed-off-by: Akshay Sakure <asakure@redhat.com>

--- tests/system/tests/test_groupadd.py
+++ tests/system/tests/test_groupadd.py
@@ -188,3 +188,22 @@ def test_groupadd__no_group(shadow: Shadow):
     with pytest.raises(ProcessError) as exc_info:
         shadow.host.conn.run("groupadd")
     assert exc_info.value.rc == 2, f"Expected return code 2(invalid usage), got {exc_info.value.rc}"
+
+
+@pytest.mark.topology(KnownTopology.Shadow)
+def test_groupadd__two_groups(shadow: Shadow):
+    """
+    :title: Groupadd command fails when two groups are mentioned
+    :setup:
+        1. None required
+    :steps:
+        1. Run groupadd command with two groupnames
+        2. Verify that groupadd command fails
+    :expectedresults:
+        1. Command with two groupnames fails
+        2. groupadd command fails with error (invalid usage)
+    :customerscenario: False
+    """
+    with pytest.raises(ProcessError) as exc_info:
+        shadow.groupadd("tgroup1 tgroup2")
+    assert exc_info.value.rc == 2, f"Expected return code 0(invalid usage), got {exc_info.value.rc}"
