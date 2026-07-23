0f097921a750e65b15c4178336fa9782b7f5073a
Akshay Sakure <asakure@redhat.com>
2026-07-23 22:14:22 +0530

    Tests: Group deletion succeeds with no gshadow entry
    This is Python transformation of the test located in
    `tests/grouptools/groupdel/02_groupdel_delete_group_no_gshadow_group/groupdel.test`
    which checks that `groupdel` succeeds when no gshadow entry is present.
    
    Signed-off-by: Akshay Sakure <asakure@redhat.com>

--- tests/system/tests/test_groupdel.py
+++ tests/system/tests/test_groupdel.py
@@ -33,3 +33,34 @@ def test_groupdel__delete_group(shadow: Shadow):
 
     gshadow_entry = shadow.tools.getent.gshadow("tgroup")
     assert gshadow_entry is None, "Group should not be found"
+
+
+@pytest.mark.topology(KnownTopology.Shadow)
+def test_groupdel__delete_group_no_gshadow_entry(shadow: Shadow):
+    """
+    :title: Group deletion with no corresponding gshadow entry
+    :setup:
+        1. Create group
+        2. Remove gshadow entry manually
+    :steps:
+        1. Delete group
+        2. Check group entry
+        3. Check gshadow entry
+    :expectedresults:
+        1. Group is deleted successfully
+        2. No group entry is found
+        3. No gshadow entry is found
+    :customerscenario: False
+    """
+    shadow.groupadd("tgroup")
+
+    shadow.fs.sed("/^tgroup:/d", "/etc/gshadow")
+
+    shadow.groupdel("tgroup")
+
+    group_entry = shadow.tools.getent.group("tgroup")
+    assert group_entry is None, "Group should not be found"
+
+    if shadow.host.features["gshadow"]:
+        gshadow_entry = shadow.tools.getent.gshadow("tgroup")
+        assert gshadow_entry is None, "Group should not be found"
