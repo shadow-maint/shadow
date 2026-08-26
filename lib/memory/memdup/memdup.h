2f14a6a19613061cafb71452ef958fee34a3804e
Alejandro Colomar <alx@kernel.org>
2026-08-26 13:54:27 +0200

    lib/: Move memdup() and strndup() under lib/memory/
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/memory/memdup/memdup.h
+++ lib/memory/memdup/memdup.h
@@ -1,9 +1,9 @@
-// SPDX-FileCopyrightText: 2025, Alejandro Colomar <alx@kernel.org>
+// SPDX-FileCopyrightText: 2025-2026, Alejandro Colomar <alx@kernel.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
 
-#ifndef SHADOW_INCLUDE_LIB_STRING_STRDUP_MEMDUP_H_
-#define SHADOW_INCLUDE_LIB_STRING_STRDUP_MEMDUP_H_
+#ifndef SHADOW_INCLUDE_LIB_MEMORY_MEMDUP_MEMDUP_H_
+#define SHADOW_INCLUDE_LIB_MEMORY_MEMDUP_MEMDUP_H_
 
 
 #include "config.h"
