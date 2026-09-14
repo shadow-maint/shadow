78fb22cb67dd5a0fc9a53a1ed460085de2e0235e
thc1006 <84045975+thc1006@users.noreply.github.com>
2026-09-15 04:29:12 +0800

    lib/subordinateio.c: list_owner_ranges(): tfix
    s/GUID/GID/
    
    Fixes: 0a7888b1fad6 (2020-06-07; "Create a new libsubid")
    Signed-off-by: thc1006 <84045975+thc1006@users.noreply.github.com>
    Reviewed-by: Alejandro Colomar <alx@kernel.org>

--- lib/subordinateio.c
+++ lib/subordinateio.c
@@ -854,7 +854,7 @@ gid_t sub_gid_find_free_range(gid_t min, gid_t max, unsigned long count)
  * int list_owner_ranges(const char *owner, enum subid_type id_type, struct subordinate_range ***ranges)
  *
  * @owner: username
- * @id_type: UID or GUID
+ * @id_type: UID or GID
  * @ranges: pointer to array of ranges into which results will be placed.
  *
  * Fills in the subuid or subgid ranges which are owned by the specified
