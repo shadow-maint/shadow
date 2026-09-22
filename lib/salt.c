7ff23fe2b80a48eed757234818b39ed8d46f9fa9
Alejandro Colomar <alx@kernel.org>
2026-09-22 21:49:44 +0200

    lib/salt.c: crypt_make_salt(): Remove dead initialization
    We set 'salt_len' later in every branch, so this value is unused.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/salt.c
+++ lib/salt.c
@@ -347,8 +347,8 @@ static /*@observer@*/const char *gensalt (size_t salt_size)
  */
 /*@observer@*/const char *crypt_make_salt (/*@null@*//*@observer@*/const char *meth, /*@null@*/void *arg)
 {
+	size_t         salt_len;
 	static char result[GENSALT_SETTING_SIZE];
-	size_t salt_len = MAX_SALT_SIZE;
 	const char *method;
 	unsigned long rounds = 0;
 
